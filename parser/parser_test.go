a1654176fd63926f969ae8c24d339b7f2742c0f7
Dekr0 <dekr0.dk@protonmail.com>
2025-04-12 05:11:25 -0600

    collect malformed sound bank when finish tests

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -3,9 +3,8 @@ package parser
 import (
 	"bytes"
 	"context"
-	"fmt"
 	"os"
-	_ "path/filepath"
+	"path/filepath"
 	"testing"
 )
 
@@ -33,49 +32,67 @@ func TestCheckHeader(t *testing.T) {
 }
 */
 
-// func TestParseBank(t *testing.T) {
-// 	banks, err := os.ReadDir("../tests/bnk")
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-// 
-// 	for _, bank := range banks {
-// 		t.Log(bank.Name())
-// 		bnkPath := filepath.Join("../tests/bnk", bank.Name())
-// 
-// 		bnk, err := ParseBank(bnkPath, context.Background())
-// 		if err != nil {
-// 			t.Fatal(err)
-// 		}
-// 
-// 		blob, err := bnk.Encode(context.Background())
-// 		if err != nil {
-// 			t.Fatal(bnkPath, err)
-// 		}
-// 
-// 		orig, err := os.ReadFile(bnkPath)
-// 		if err != nil {
-// 			t.Fatal(bnkPath, err)
-// 		}
-// 
-// 		if bytes.Compare(blob, orig) != 0 {
-// 			if len(blob) > len(orig) {
-// 				for i := range orig {
-// 					if blob[i] != orig[i] {
-// 						t.Fatalf(bnkPath, "Byte difference at %d. Original: %d, Received: %d\n", i, orig[i], blob[i])
-// 					}
-// 				}
-// 			} else {
-// 				for i := range blob {
-// 					if blob[i] != orig[i] {
-// 						t.Fatalf(bnkPath, "Byte difference at %d. Original: %d, Received: %d\n", i, orig[i], blob[i])
-// 					}
-// 				}
-// 			}
-// 		}
-// 	}
-// }
+type malformedSoundbank struct {
+	name string
+	err  error
+}
 
+func TestParseBank(t *testing.T) {
+	banks, err := os.ReadDir("../tests/bnk")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	excludes := []*malformedSoundbank{}
+
+	for _, bank := range banks {
+		t.Log(bank.Name())
+		bnkPath := filepath.Join("../tests/bnk", bank.Name())
+
+		bnk, err := ParseBank(bnkPath, context.Background())
+		if err != nil {
+			if err == MissingBKHDError || err == MissingDATAError || err == MissingDIDXError || err == MissingHIRCError {
+				excludes = append(excludes, &malformedSoundbank{bank.Name(), err})
+				continue
+			} else {
+				t.Fatal(err)
+			}
+		}
+
+		blob, err := bnk.Encode(context.Background())
+		if err != nil {
+			t.Fatal(bnkPath, err)
+		}
+
+		orig, err := os.ReadFile(bnkPath)
+		if err != nil {
+			t.Fatal(bnkPath, err)
+		}
+
+		if bytes.Compare(blob, orig) != 0 {
+			if len(blob) > len(orig) {
+				for i := range orig {
+					if blob[i] != orig[i] {
+						t.Fatalf("%s: Byte difference at %d. Original: %d, Received: %d\n", bank.Name(), i, orig[i], blob[i])
+					}
+				}
+			} else {
+				for i := range blob {
+					if blob[i] != orig[i] {
+						t.Fatalf("%s: Byte difference at %d. Original: %d, Received: %d\n", bank.Name(), i, orig[i], blob[i])
+					}
+				}
+			}
+		}
+	}
+
+	t.Log("Malformed sound bank")
+	for _, exclude := range excludes {
+		t.Log(exclude.name, exclude.err)
+	}
+}
+
+/*
 func TestParseBankEdgeCase(t *testing.T) {
 	edgeCases := []string{
 		"../tests/bnk/content_audio_haz_explosivemushroom.bnk",
@@ -114,3 +131,4 @@ func TestParseBankEdgeCase(t *testing.T) {
 		}
 	}
 }
+*/
