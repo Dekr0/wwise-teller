bd4c996a8ba1b920b5b62d3bf127da852486e60d
Dekr0 <dekr0.dk@protonmail.com>
2025-05-03 03:20:58 -0600

    fix bound check condition; add more methods to read different struct and data types

--- wio/reader.go
+++ wio/reader.go
@@ -365,7 +365,7 @@ func (r *InPlaceReader) U8Unsafe() uint8 {
 }
 
 func (r *InPlaceReader) U8() (uint8, error) {
-	if r.Len() <= 0 {
+	if r.Len() < 1 {
 		return 0, io.ErrShortBuffer
 	}
 	var v uint8
@@ -384,7 +384,7 @@ func (r *InPlaceReader) I8Unsafe() int8 {
 }
 
 func (r *InPlaceReader) I8() (int8, error) {
-	if r.Len() <= 0 {
+	if r.Len() < 1 {
 		return 0, io.ErrShortBuffer
 	}
 	var v int8
@@ -403,7 +403,7 @@ func (r *InPlaceReader) U16Unsafe() uint16 {
 }
 
 func (r *InPlaceReader) U16() (uint16, error) {
-	if r.Len() <= 0 {
+	if r.Len() < 2 {
 		return 0, io.ErrShortBuffer
 	}
 	var v uint16
@@ -422,7 +422,7 @@ func (r *InPlaceReader) I16Unsafe() int16 {
 }
 
 func (r *InPlaceReader) I16() (int16, error) {
-	if r.Len() <= 0 {
+	if r.Len() < 2 {
 		return 0, io.ErrShortBuffer
 	}
 	var v int16
@@ -441,7 +441,7 @@ func (r *InPlaceReader) U32Unsafe() uint32 {
 }
 
 func (r *InPlaceReader) U32() (uint32, error) {
-	if r.Len() <= 0 {
+	if r.Len() < 4 {
 		return 0, io.ErrShortBuffer
 	}
 	var v uint32
@@ -460,7 +460,7 @@ func (r *InPlaceReader) I32Unsafe() int32 {
 }
 
 func (r *InPlaceReader) I32() (int32, error) {
-	if r.Len() <= 0 {
+	if r.Len() < 4 {
 		return 0, io.ErrShortBuffer
 	}
 	var v int32
@@ -479,7 +479,7 @@ func (r *InPlaceReader) F32Unsafe() float32 {
 }
 
 func (r *InPlaceReader) F32() (float32, error) {
-	if r.Len() <= 0 {
+	if r.Len() < 4 {
 		return 0, io.ErrShortBuffer
 	}
 	var v float32
@@ -491,8 +491,31 @@ func (r *InPlaceReader) F32() (float32, error) {
 	return v, err
 }
 
+func (r *InPlaceReader) FourCCNoCopyUnsafe() ([]byte) {
+	b, err := r.FourCCNoCopy()
+	if err != nil { panic(err) }
+	return b
+}
+
+func (r *InPlaceReader) FourCCNoCopy() ([]byte, error) {
+	if r.Len() < 4 {
+		return nil, io.ErrShortBuffer
+	}
+	b := r.Buff[r.curr:r.curr + 4]
+	r.curr += 4
+	return b, nil
+}
+
+func (r *InPlaceReader) FourCCUnsafe() []byte {
+	b, err := r.FourCC()
+	if err != nil {
+		panic(err)
+	}
+	return b
+}
+
 func (r *InPlaceReader) FourCC() ([]byte, error) {
-	if r.Len() <= 0 {
+	if r.Len() < 4 {
 		return nil, io.ErrShortBuffer
 	}
 	
@@ -508,7 +531,7 @@ func (r *InPlaceReader) U64Unsafe() uint64 {
 }
 
 func (r *InPlaceReader) U64() (uint64, error) {
-	if r.Len() <= 0 {
+	if r.Len() < 8 {
 		return 0, io.ErrShortBuffer
 	}
 	var v uint64
@@ -527,7 +550,7 @@ func (r *InPlaceReader) I64Unsafe() int64 {
 }
 
 func (r *InPlaceReader) I64() (int64, error) {
-	if r.Len() <= 0 {
+	if r.Len() < 8 {
 		return 0, io.ErrShortBuffer
 	}
 	var v int64
@@ -537,4 +560,39 @@ func (r *InPlaceReader) I64() (int64, error) {
 		r.curr += 8
 	} 
 	return v, err
+}
+
+func (r *InPlaceReader) ReadNoCopyUnsafe(n uint) []byte {
+	b, err := r.ReadNoCopy(n)
+	if err != nil {
+		panic(err)
+	}
+	return b
+}
+
+func (r *InPlaceReader) ReadNoCopy(n uint) ([]byte, error) {
+	if r.Len() < n {
+		return nil, io.ErrShortBuffer
+	}
+	b := r.Buff[r.curr:r.curr + n]
+	r.curr += n
+	return b, nil
+}
+
+func (r *InPlaceReader) ReadUnsafe(n uint) []byte {
+	b, err := r.Read(n)
+	if err != nil {
+		panic(err)
+	}
+	return b
+}
+
+func (r *InPlaceReader) Read(n uint) ([]byte, error) {
+	if r.Len() < n {
+		return nil, io.ErrShortBuffer
+	}
+	
+	b := bytes.Clone(r.Buff[r.curr:r.curr + n])
+	r.curr += n
+	return b, nil
 }
