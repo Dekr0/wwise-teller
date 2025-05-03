0bc93d4718b79f732c4828b437b80f4bd2edaaf8
Dekr0 <dekr0.dk@protonmail.com>
2025-05-03 14:26:09 -0600

    bug fixes and new functions
    - Forget to concate sound bank name with the destnation path
    - Added callback function to save patches
    - Add more context in callback message

--- ui/cb_bank_explorer.go
+++ ui/cb_bank_explorer.go
@@ -7,6 +7,7 @@ import (
 	"path/filepath"
 	"time"
 
+	"github.com/Dekr0/wwise-teller/integration/helldivers"
 	"github.com/Dekr0/wwise-teller/ui/async"
 	"github.com/Dekr0/wwise-teller/utils"
 )
@@ -57,14 +58,15 @@ func saveSoundBankFunc(
 	loop *async.EventLoop,
 	bnkMngr *BankManager,
 	saveTab *bankTab,
+	saveName string,
 ) func(string) {
 	return func(path string) {
 		timeout, cancel := context.WithTimeout(
-			context.Background(), time.Second * 30,
+			context.Background(), time.Second * 4,
 		)
 
-		onProcMsg := fmt.Sprintf("Saving sound bank to %s", path)
-		onDoneMsg := fmt.Sprintf("Saved sound bank to %s", path)
+		onProcMsg := fmt.Sprintf("Saving sound bank %s to %s", saveName, path)
+		onDoneMsg := fmt.Sprintf("Saved sound bank %s to %s", saveName, path)
 
 		if err := loop.QTask(timeout, cancel,
 			onProcMsg, onDoneMsg,
@@ -75,24 +77,81 @@ func saveSoundBankFunc(
 
 				if data, err := saveTab.encode(ctx); err != nil {
 					slog.Error(
-						fmt.Sprintf("Failed to encode sound bank %s", filepath.Base(path)),
+						fmt.Sprintf("Failed to encode sound bank %s", saveName),
 						"error", err,
 					)
 				} else {
-					if err := utils.SaveFileWithRetry(data, path); err != nil {
+					if err := utils.SaveFileWithRetry(
+						data, 
+						filepath.Join(path, filepath.Base(saveName)),
+					); err != nil {
 						slog.Error(
-							fmt.Sprintf("Failed to save sound bank to %s", path),
+							fmt.Sprintf("Failed to save sound bank %s to %s",
+								saveName, path),
 							"error", err,
 						)
 					} else {
 						slog.Info(onDoneMsg)
 					}
 				}
-
 				bnkMngr.writeLock.Store(false)
 			},
 		); err != nil {
 			slog.Error(fmt.Sprintf("Failed to save sound bank to %s", path),
+				"error", err,
+			)
+		}
+	}
+}
+
+func HD2PatchFunc(
+	loop *async.EventLoop,
+	bnkMngr *BankManager,
+	saveTab *bankTab,
+	saveName string,
+) func(string) {
+	return func(path string) {
+		timeout, cancel := context.WithTimeout(
+			context.Background(), time.Second * 4,
+		)
+		
+		onProcMsg := fmt.Sprintf("Saving sound bank %s to HD2 patch %s",
+			saveName, path)
+		onDoneMsg := fmt.Sprintf("Saved sound bank %s to HD2 patch %s",
+			saveName, path)
+
+		if err := loop.QTask(timeout, cancel, onProcMsg, onDoneMsg, 
+			func(ctx context.Context) {
+				slog.Info(onProcMsg)
+				bnkMngr.writeLock.Store(true)
+				defer bnkMngr.writeLock.Store(false)
+				bnkData, err := saveTab.encode(ctx)
+				if err != nil {
+					slog.Error(
+						fmt.Sprintf("Failed to encode sound bank %s", saveName),
+						"error", err,
+					)
+					return
+				}
+				meta := saveTab.bank.META()
+				if meta == nil {
+					slog.Error(
+						fmt.Sprintf("Sound bank %s is missing integration data.",
+							saveName),
+					)
+					return
+				}
+				if err := helldivers.GenHelldiversPatch(
+					ctx, bnkData, meta.Data, path,
+				); err != nil {
+					slog.Error(fmt.Sprintf("Failed to write HD2 patch to %s", path))
+				} else {
+					slog.Info(onDoneMsg)
+				}
+			},
+		);
+		   err != nil {
+			slog.Error(fmt.Sprintf("Failed to save HD2 patch to %s", path),
 				"error", err,
 			)
 		}
