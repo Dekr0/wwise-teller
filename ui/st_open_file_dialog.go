e9a0d64bcec0a675b1554f2d3b456e2095953ce3
Dekr0 <dekr0.dk@protonmail.com>
2025-05-04 23:06:23 -0600

    Public to private

--- ui/st_open_file_dialog.go
+++ ui/st_open_file_dialog.go
@@ -31,48 +31,48 @@ func NewOpenFileDialog(
 	}, nil
 }
 
-func (d *OpenFileDialog) ResetSelection() {
+func (d *OpenFileDialog) resetSelection() {
 	d.storage.Clear()
 }
 
-func (d *OpenFileDialog) Filter() {
+func (d *OpenFileDialog) filter() {
 	d.fs.filter()
-	d.ResetSelection()
+	d.resetSelection()
 }
 
-func (d *OpenFileDialog) IsFocusDir(n int) bool {
+func (d *OpenFileDialog) isFocusDir(n int) bool {
 	if n >= 0 && n < len(d.fs.filtered) {
 		return d.fs.filtered[n].entry.IsDir()
 	}
 	return false
 }
 
-func (f *OpenFileDialog) CdFocus(n int) error {
+func (f *OpenFileDialog) cdFocus(n int) error {
 	if n >= 0 && n < len(f.fs.filtered) {
 		if err := f.fs.cd(f.fs.filtered[n].entry.Name()); err != nil {
 			return err
 		}
-		f.ResetSelection()
+		f.resetSelection()
 	}
 	return nil
 }
 
-func (d *OpenFileDialog) CdParent() error {
+func (d *OpenFileDialog) cdParent() error {
 	if err := d.fs.cdParent(); err != nil {
 		return err
 	}
-	d.ResetSelection()
+	d.resetSelection()
 	return nil
 }
 
-func (d *OpenFileDialog) OpenSelective() {
+func (d *OpenFileDialog) openSelective() {
 	paths := []string{}
 	for i, e := range d.fs.filtered {
 		if d.storage.Contains(imgui.ID(i))  {
 			if !d.fs.dirOnly && e.entry.IsDir() {
 				continue
 			}
-			paths = append(paths, filepath.Join(d.fs.Pwd, e.entry.Name()))
+			paths = append(paths, filepath.Join(d.fs.pwd, e.entry.Name()))
 		}
 	}
 	d.callback(paths)
