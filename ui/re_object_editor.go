022364ae85e984e6630876b1ed9460077268ab01
Dekr0 <dekr0.dk@protonmail.com>
2025-05-24 00:56:40 -0600

    Ability to change source ID of sound object

--- ui/re_object_editor.go
+++ ui/re_object_editor.go
@@ -111,6 +111,7 @@ func renderSwitchCntr(t *bankTab, o *wwise.SwitchCntr) {
 }
 
 func renderSound(t *bankTab, o *wwise.Sound) {
+	renderBankSourceData(t, o)
 	renderBaseParam(t, o)
 }
 
@@ -121,6 +122,96 @@ func renderUnknown(o *wwise.Unknown) {
 			wwise.HircTypeName[o.HircType()],
 		),
 	)
+}
+
+func renderBankSourceData(t *bankTab, o *wwise.Sound) {
+	if imgui.TreeNodeExStr("Bank Source Data") {
+		bsd := o.BankSourceData
+		imgui.Text(fmt.Sprintf("Plugin Type ID: %d", bsd.PluginType()))
+		imgui.Text(fmt.Sprintf("Plugin Company ID: %d", bsd.Company()))
+		imgui.Text("Stream Type: " + wwise.SourceType[bsd.StreamType])
+
+		renderChangeSourceQuery(t, &bsd)
+		imgui.SameLine()
+		renderChangeSourceTable(t)
+
+		imgui.BeginDisabled()
+
+		languageSpecific := bsd.LanguageSpecific()
+		imgui.Checkbox("Language specific", &languageSpecific)
+
+		nonCacheAble := bsd.NonCacheable()
+		imgui.Checkbox("Non cacheable", &nonCacheAble)
+
+		imgui.EndDisabled()
+		imgui.TreePop()
+	}
+}
+
+func renderChangeSourceQuery(t *bankTab, bsd *wwise.BankSourceData) {
+	size := imgui.NewVec2(imgui.ContentRegionAvail().X * 0.30, 128)
+	imgui.BeginChildStrV("ChangeSourceChild", size, 0, 0)
+
+	imgui.Text("Filtered by source ID")
+	if imgui.InputTextWithHint("##Filtered by source ID", "", &t.rewireSidQuery, 0, nil) {
+		t.filterRewireQuery()
+	}
+
+	preview := strconv.FormatUint(uint64(bsd.SourceID), 10)
+	imgui.Text("Source ID")
+	if imgui.BeginComboV("##Source ID", preview, 0) {
+		for _, m := range t.filteredMediaIndexs {
+			selected := bsd.SourceID == m.Sid
+			preview := strconv.FormatUint(uint64(m.Sid), 10)
+			if imgui.SelectableBoolPtr(preview, &selected) {
+				bsd.SourceID = m.Sid
+				bsd.InMemoryMediaSize = m.Size
+			}
+			if selected {
+				imgui.SetItemDefaultFocus()
+			}
+		}
+		imgui.EndCombo()
+	}
+
+	imgui.EndChild()
+}
+
+func renderChangeSourceTable(t *bankTab) {
+	imgui.BeginChildStr("ChangeSourceChildTable")
+
+	flags := imgui.TableFlagsResizable   |
+			 imgui.TableFlagsReorderable |
+		     imgui.TableFlagsRowBg       |
+		     imgui.TableFlagsBordersH    |
+			 imgui.TableFlagsBordersV    |
+		     imgui.TableFlagsScrollY
+	outerSize := imgui.NewVec2(0, 0)
+	if imgui.BeginTableV("SourceTable", 2, flags, outerSize, 0) {
+		imgui.TableSetupColumn("Source ID")
+		imgui.TableSetupColumn("Media Size")
+		imgui.TableSetupScrollFreeze(0, 1)
+		imgui.TableHeadersRow()
+
+		clipper:= imgui.NewListClipper()
+		clipper.Begin(int32(len(t.filteredMediaIndexs)))
+		for clipper.Step() {
+			for n := clipper.DisplayStart(); n < clipper.DisplayEnd(); n++ {
+				m := t.filteredMediaIndexs[n]
+				
+				imgui.TableNextRow()
+
+				imgui.TableSetColumnIndex(0)
+				imgui.Text(strconv.FormatUint(uint64(m.Sid), 10))
+
+				imgui.TableSetColumnIndex(1)
+				imgui.Text(strconv.FormatUint(uint64(m.Size), 10))
+			}
+		}
+		imgui.EndTable()
+	}
+
+	imgui.EndChild()
 }
 
 func renderBaseParam(t *bankTab, o wwise.HircObj) {
