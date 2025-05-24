79c5675b17e4689fa8f9bbe73ff0433999ccca52
Dekr0 <dekr0.dk@protonmail.com>
2025-05-24 00:50:45 -0600

    structure for music track and music segment

--- wwise/music_segment.go
+++ wwise/music_segment.go
@@ -5,23 +5,48 @@ import "github.com/Dekr0/wwise-teller/wio"
 type MusicSegment struct {
 	HircObj
 
-	Id   uint32
-	Data []byte
+	Id             uint32
+	OverwriteFlags uint8
+	BaseParam      BaseParameter
+	Children       Container
+	MeterInfo      MeterInfo
+	// NumStingers uint32
+	Stingers []Stinger
+	Duration float64
+	// NumMarkers  uint32
+	Markers []MusicSegmentMarker
 }
 
 func (h *MusicSegment) Encode() []byte {
-	dataSize := h.DataSize()
+	dataSize := h.Size()
 	size := SizeOfHircObjHeader + dataSize
 	w := wio.NewWriter(uint64(size))
 	w.AppendByte(uint8(HircTypeMusicSegment))
 	w.Append(dataSize)
 	w.Append(h.Id)
-	w.AppendBytes(h.Data)
+	w.AppendByte(h.OverwriteFlags)
+	w.AppendBytes(h.BaseParam.Encode())
+	w.AppendBytes(h.Children.Encode())
+	w.Append(h.MeterInfo)
+	w.Append(uint32(len(h.Stingers)))
+	for _, s := range h.Stingers {
+		w.Append(s)
+	}
+	w.Append(h.Duration)
+	w.Append(uint32(len(h.Markers)))
+	for _, m := range h.Markers {
+		w.AppendBytes(m.Encode())
+	}
 	return w.BytesAssert(int(size))
 }
 
-func (h *MusicSegment) DataSize() uint32 {
-	return uint32(4 + len(h.Data))
+func (h *MusicSegment) Size() uint32 {
+	dataSize := 4 + 1 + h.BaseParam.Size() + h.Children.Size() + SizeOfMeterInfo + 4 + SizeOfStinger*uint32(len(h.Stingers))
+	dataSize += 8 + 4
+	for _, m := range h.Markers {
+		dataSize += uint32(m.Size())
+	}
+	return dataSize
 }
 
 func (h *MusicSegment) BaseParameter() *BaseParameter { return nil }
@@ -30,12 +55,53 @@ func (h *MusicSegment) HircType() HircType { return HircTypeMusicSegment }
 
 func (h *MusicSegment) HircID() (uint32, error) { return h.Id, nil }
 
-func (h *MusicSegment) IsCntr() bool { return false }
+func (h *MusicSegment) IsCntr() bool { return true }
 
-func (h *MusicSegment) NumLeaf() int { return 0 }
+func (h *MusicSegment) NumLeaf() int { return len(h.Children.Children) }
 
-func (h *MusicSegment) ParentID() int { return 0 }
+func (h *MusicSegment) ParentID() uint32 { return h.BaseParam.DirectParentId }
 
 func (h *MusicSegment) AddLeaf(o HircObj) { panic("") }
 
 func (h *MusicSegment) RemoveLeaf(o HircObj) { panic("") }
+
+const SizeOfMeterInfo = 23
+
+type MeterInfo struct {
+	GridPeriod         float64
+	GridOffset         float64
+	Tempo              float32
+	TimeSigNumBeatsBar uint8
+	TimeSigBeatVal     uint8
+	MeterInfoFlag      uint8
+}
+
+const SizeOfStinger = 24
+
+type Stinger struct {
+	TriggerID           uint32
+	SegmentID           uint32
+	SyncPlayAt          uint32
+	CueFilterHash       uint32
+	DontRepeatTime      int32
+	NumSegmentLookAhead uint32
+}
+
+type MusicSegmentMarker struct {
+	ID         uint32
+	Position   float64
+	MarkerName []byte
+}
+
+func (m *MusicSegmentMarker) Encode() []byte {
+	dataSize := m.Size()
+	w := wio.NewWriter(uint64(dataSize))
+	w.Append(m.ID)
+	w.Append(m.Position)
+	w.AppendBytes(m.MarkerName)
+	return w.BytesAssert(int(dataSize))
+}
+
+func (m *MusicSegmentMarker) Size() uint8 {
+	return 4 + 8 + uint8(len(m.MarkerName))
+}
---
+++ wwise/music_track.go
@@ -0,0 +1,171 @@
+package wwise
+
+import (
+	wio "github.com/Dekr0/wwise-teller/wio"
+)
+
+type MusicTrack struct {
+	HircObj
+
+	Id              uint32
+	OverrideFlags   uint8
+	// NumSources
+	Sources         []BankSourceData
+	// NumPlayListItem
+	PlayListItems   []MusicTrackPlayListItem
+	NumSubTrack     uint32
+	// NumClipAutomationItem uint32
+	ClipAutomations []ClipAutomation
+	BaseParam       BaseParameter
+
+	TrackType       uint8
+	SwitchParam     MusicTrackSwitchParam
+	TransitionParam MusicTrackTransitionParam
+
+	LookAheadTime   int32
+}
+
+func (h *MusicTrack) Encode() []byte {
+	dataSize := h.DataSize()
+	size := SizeOfHircObjHeader + dataSize
+	w := wio.NewWriter(uint64(size))
+	w.AppendByte(uint8(HircTypeMusicTrack))
+	w.Append(dataSize)
+	w.Append(h.Id)
+	w.AppendByte(h.OverrideFlags)
+	w.Append(uint32(len(h.Sources)))
+	for _, s := range h.Sources {
+		w.AppendBytes(s.Encode())
+	}
+	w.Append(uint32(len(h.PlayListItems)))
+	for _, p := range h.PlayListItems {
+		w.Append(p)
+	}
+	if len(h.PlayListItems) > 0 {
+		w.Append(h.NumSubTrack)
+	}
+	w.Append(uint32(len(h.ClipAutomations)))
+	for _, c := range h.ClipAutomations {
+		w.AppendBytes(c.Encode())
+	}
+	w.AppendBytes(h.BaseParam.Encode())
+	w.AppendByte(h.TrackType)
+	if h.UseSwitchAndTransition() {
+		w.AppendBytes(h.SwitchParam.Encode())
+		w.Append(h.TransitionParam)
+	}
+	w.Append(h.LookAheadTime)
+	return w.BytesAssert(int(size))
+}
+
+func (h *MusicTrack) DataSize() uint32 {
+	dataSize := uint32(4 + 1 + 4)
+	for _, s := range h.Sources {
+		dataSize += s.Size()
+	}
+	dataSize += 4 + uint32(len(h.PlayListItems)) * SizeOfMusicTrackPlayListItem
+	if len(h.PlayListItems) > 0 {
+		dataSize += 4
+	}
+	dataSize += 4
+	for _, c := range h.ClipAutomations {
+		dataSize += uint32(c.Size())
+	}
+	dataSize += h.BaseParam.Size() + 1 
+	if h.UseSwitchAndTransition() {
+		dataSize += uint32(h.SwitchParam.Size()) + SizeOfMusicTrackTransitionParam 
+	}
+	dataSize += 4
+	return dataSize
+}
+
+func (h *MusicTrack) BaseParameter() *BaseParameter { return nil }
+
+func (h *MusicTrack) HircType() HircType { return HircTypeMusicTrack }
+
+func (h *MusicTrack) HircID() (uint32, error) { return h.Id, nil }
+
+func (h *MusicTrack) IsCntr() bool { return false }
+
+func (h *MusicTrack) NumLeaf() int { return 0 }
+
+func (h *MusicTrack) ParentID() uint32 { return h.BaseParam.DirectParentId }
+
+func (h *MusicTrack) AddLeaf(o HircObj) { panic("") }
+
+func (h *MusicTrack) RemoveLeaf(o HircObj) { panic("") }
+
+func (h *MusicTrack) UseSwitchAndTransition() bool {
+	return h.TrackType == 0x03
+}
+
+const SizeOfMusicTrackPlayListItem = 44
+type MusicTrackPlayListItem struct {
+	TrackID         uint32
+	SourceID        uint32
+	EventID         uint32
+	PlayAt          float64
+	BeginTrimOffset float64
+	EndTrimOffset   float64
+	SrcDuration     float64
+}
+
+type ClipAutomation struct {
+	ClipIndex       uint32
+	AutoType        uint32
+	// NumPoints       uint32
+	RTPCGraphPoints []RTPCGraphPoint
+}
+
+func (c *ClipAutomation) Encode() []byte {
+	dataSize := c.Size()
+	w := wio.NewWriter(uint64(dataSize))
+	w.Append(c.ClipIndex)
+	w.Append(c.AutoType)
+	w.Append(uint32(len(c.RTPCGraphPoints)))
+	for _, r := range c.RTPCGraphPoints {
+		w.Append(r)
+	}
+	return w.BytesAssert(int(dataSize))
+}
+
+func (c *ClipAutomation) Size() uint16 {
+	return 4 + 4 + 4 + uint16(len(c.RTPCGraphPoints)) * SizeOfRTPCGraphPoint
+}
+
+type MusicTrackSwitchParam struct {
+	GroupType        uint8
+	GroupID          uint32
+	DefaultSwitch    uint32
+	// NumSwitchAssoc uint32
+	SwitchAssociates []uint32
+}
+
+func (m *MusicTrackSwitchParam) Encode() []byte {
+	dataSize := m.Size()
+	w := wio.NewWriter(uint64(dataSize))
+	w.Append(m.GroupType)
+	w.Append(m.GroupID)
+	w.Append(m.DefaultSwitch)
+	w.Append(uint32(len(m.SwitchAssociates)))
+	for _, s := range m.SwitchAssociates {
+		w.Append(s)
+	}
+	return w.BytesAssert(int(dataSize))
+}
+
+func (m *MusicTrackSwitchParam) Size() uint16 {
+	return 1 + 4 + 4 + 4 + uint16(len(m.SwitchAssociates)) * 4
+}
+
+const SizeOfMusicTrackTransitionParam = 32
+type MusicTrackTransitionParam struct {
+	SrcTransitionTime  int32
+	SrcFadeCurve       uint32
+	SrcFadeOffset      int32
+	SyncType           uint32
+	CueFilterHash      uint32	
+	DestTransitionTime int32
+	DestFadeCurve      uint32
+	DestFadeOffset     int32
+}
