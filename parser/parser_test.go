5059520b04d7c3e789d8611028f356fee6812d9c
Dekr0 <dekr0.dk@protonmail.com>
2025-05-24 00:56:09 -0600

    Test for music segment and music track

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 	"time"
 )
@@ -15,7 +16,7 @@ type malformedSoundbank struct {
 	err  error
 }
 
-func testParseBank(t *testing.T) {
+func TestParseBank(t *testing.T) {
 	banks, err := os.ReadDir("../tests/bnk")
 	if err != nil {
 		t.Fatal(err)
@@ -27,7 +28,7 @@ func testParseBank(t *testing.T) {
 		t.Log(bank.Name())
 		bnkPath := filepath.Join("../tests/bnk", bank.Name())
 
-		ctx, cancel := context.WithTimeout(context.Background(), time.Second * 4)
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*360)
 		bnk, err := ParseBank(bnkPath, ctx)
 		if err != nil {
 			cancel()
@@ -40,7 +41,7 @@ func testParseBank(t *testing.T) {
 		}
 		cancel()
 
-		ctx, cancel = context.WithTimeout(context.Background(), time.Second * 4)
+		ctx, cancel = context.WithTimeout(context.Background(), time.Second*4)
 		blob, err := bnk.Encode(ctx)
 		if err != nil {
 			cancel()
@@ -80,12 +81,79 @@ func testParseBank(t *testing.T) {
 	}
 }
 
-func TestHelldiversIntegration(t *testing.T) {
-	_, err := ParseBank(
-		"../integration/helldivers/content_audio_stratagems_shared.st_bnk", 
-		context.Background(),
-	)
+func TestParseMusicBank(t *testing.T) {
+	banks, err := os.ReadDir("../tests/bnk")
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	excludes := []*malformedSoundbank{}
+
+	for _, bank := range banks {
+		if !strings.HasPrefix(bank.Name(), "music_") {
+			continue
+		}
+		t.Log(bank.Name())
+
+		bnkPath := filepath.Join("../tests/bnk", bank.Name())
+
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*360)
+		bnk, err := ParseBank(bnkPath, ctx)
+		if err != nil {
+			cancel()
+			if err == NoBKHD || err == NoDATA || err == NoDIDX || err == NoHIRC {
+				excludes = append(excludes, &malformedSoundbank{bank.Name(), err})
+				continue
+			} else {
+				t.Fatal(err)
+			}
+		}
+		cancel()
+
+		ctx, cancel = context.WithTimeout(context.Background(), time.Second*360)
+		blob, err := bnk.Encode(ctx)
+		if err != nil {
+			cancel()
+			t.Fatal(bnkPath, err)
+		}
+		cancel()
+
+		orig, err := os.ReadFile(bnkPath)
+		if err != nil {
+			t.Fatal(bnkPath, err)
+		}
+
+		if bytes.Compare(blob, orig) != 0 {
+			if len(blob) > len(orig) {
+				for i := range orig {
+					if blob[i] != orig[i] {
+						l, _ := bnk.HIRC().Encode(context.Background())
+						fmt.Println(len(l))
+						t.Fatalf("%s: Byte difference at %d. Original: %d, Received: %d\n", bank.Name(), i, orig[i], blob[i])
+					}
+				}
+			} else {
+				for i := range blob {
+					if blob[i] != orig[i] {
+						l, _ := bnk.HIRC().Encode(context.Background())
+						fmt.Println(len(l))
+						t.Fatalf("%s: Byte difference at %d. Original: %d, Received: %d\n", bank.Name(), i, orig[i], blob[i])
+					}
+				}
+			}
+		}
+	}
+
+	t.Log("Malformed sound bank")
+	for _, exclude := range excludes {
+		t.Log(exclude.name, exclude.err)
+	}
+}
+
+func TestParseBankCheckSize(t *testing.T) {
+	bnk, err := ParseBank("../tests/bnk/wep_mg43.bnk", context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+	bnk.Encode(context.Background())
 }
