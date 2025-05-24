087f45ee33e5674a4981a3a157a2ec6aad01f17f
Dekr0 <dekr0.dk@protonmail.com>
2025-05-24 08:21:48 -0600

    music track properties

--- ui/re_object_editor.go
+++ ui/re_object_editor.go
@@ -78,6 +78,8 @@ func renderHircTab(t *bankTab, i int, h wwise.HircObj) {
 			renderActorMixer(t, h.(*wwise.ActorMixer))
 		case *wwise.LayerCntr:
 			renderLayerCntr(t, h.(*wwise.LayerCntr))
+		case *wwise.MusicTrack:
+			renderMusicTrack(t, h.(*wwise.MusicTrack))
 		case *wwise.RanSeqCntr:
 			renderRanSeqCntr(t, h.(*wwise.RanSeqCntr))
 		case *wwise.SwitchCntr:
@@ -98,6 +100,138 @@ func renderActorMixer(t *bankTab, o *wwise.ActorMixer) {
 
 func renderLayerCntr(t *bankTab, o *wwise.LayerCntr) {
 	renderBaseParam(t, o)
+}
+
+func renderMusicTrack(t *bankTab, o *wwise.MusicTrack) {
+	if imgui.TreeNodeExStr("Music Override Flags") {
+		overrideParentMIDITempo := o.OverrideParentMIDITempo()
+		if imgui.Checkbox("Override Parent MIDI Tempo", &overrideParentMIDITempo) {
+			o.SetOverrideParentMIDITempo(overrideParentMIDITempo)
+		}
+
+		overrideParentMIDITarget := o.OverrideParentMIDITarget()
+		if imgui.Checkbox("Override Parent MIDI Target", &overrideParentMIDITarget) {
+			o.SetOverrideParentMIDITarget(overrideParentMIDITarget)
+		}
+
+		midiTargetTypeBus := o.MidiTargetTypeBus()
+		if imgui.Checkbox("MIDI Target Type Bus", &midiTargetTypeBus) {
+			o.SetMidiTargetTypeBus(midiTargetTypeBus)
+		}
+		imgui.TreePop()
+	}
+	// renderBankSourceData ???
+	renderMusicTrackPlayList(t, o)
+	renderClipAutomation(t, o)
+	renderBaseParam(t, o)
+}
+
+func renderMusicTrackPlayList(t *bankTab, o *wwise.MusicTrack) {
+	if imgui.TreeNodeExStr("Music Track Play List") {
+		flags := imgui.TableFlagsResizable   |
+				 imgui.TableFlagsReorderable |
+			     imgui.TableFlagsRowBg       |
+			     imgui.TableFlagsBordersH    |
+				 imgui.TableFlagsBordersV    |
+			     imgui.TableFlagsScrollY
+		outerSize := imgui.NewVec2(0, 0)
+		if imgui.BeginTableV("MusicTrackPlayListTable", 7, flags, outerSize, 0) {
+			imgui.TableSetupColumn("Track ID")
+			imgui.TableSetupColumn("Source ID")
+			imgui.TableSetupColumn("Event ID")
+			imgui.TableSetupColumn("Play At")
+			imgui.TableSetupColumn("Begin Trim Offset")
+			imgui.TableSetupColumn("End Trim Offset")
+			imgui.TableSetupColumn("Source Duration")
+			imgui.TableSetupScrollFreeze(0, 1)
+			imgui.TableHeadersRow()
+			
+			for i := range o.PlayListItems {
+				p := &o.PlayListItems[i]
+				imgui.TableNextRow()
+
+				imgui.TableSetColumnIndex(0)
+				imgui.Text(strconv.FormatUint(uint64(p.TrackID), 10))
+
+				imgui.TableSetColumnIndex(1)
+				imgui.Text(strconv.FormatUint(uint64(p.SourceID), 10))
+
+				imgui.TableSetColumnIndex(2)
+				imgui.Text(strconv.FormatUint(uint64(p.EventID), 10))
+
+				imgui.TableSetColumnIndex(3)
+				imgui.SetNextItemWidth(-1)
+				playAt := float32(p.PlayAt)
+				if imgui.InputFloat(fmt.Sprintf("##%dPlayAt", o.Id), &playAt) {
+					p.PlayAt = float64(playAt)
+				}
+
+				imgui.TableSetColumnIndex(4)
+				imgui.SetNextItemWidth(-1)
+				beginTrimOffset := float32(p.BeginTrimOffset)
+				if imgui.InputFloat(fmt.Sprintf("##%dBeginTrimOffset", o.Id), &beginTrimOffset) {
+					p.BeginTrimOffset = float64(beginTrimOffset)
+				}
+
+				imgui.TableSetColumnIndex(5)
+				imgui.SetNextItemWidth(-1)
+				endTrimOffset := float32(p.EndTrimOffset)
+				if imgui.InputFloat(fmt.Sprintf("##%dEndTrimOffset", o.Id), &endTrimOffset) {
+					p.EndTrimOffset = float64(endTrimOffset)
+				}
+
+				imgui.TableSetColumnIndex(6)
+				imgui.Text(strconv.FormatFloat(p.SrcDuration, 'f', -1, 64))
+			}
+			imgui.EndTable()
+		}
+		imgui.Text(fmt.Sprintf("Number of sub-track: %d", o.NumSubTrack))
+		imgui.TreePop()
+	}
+}
+
+func renderClipAutomation(t *bankTab, o *wwise.MusicTrack) {
+	if imgui.TreeNodeExStr("Clip Automations") {
+		if imgui.Button("Add New Automation") {
+			o.AddNewAutomation()
+		}
+		var delete func() = nil
+		for i := range o.ClipAutomations {
+			c := &o.ClipAutomations[i]
+			imgui.PushIDStr(fmt.Sprintf("RemoveAutomation%d", i))
+			if imgui.Button("X") {
+				delete = bindDeleteClipAutomation(o, i)
+			}
+			imgui.PopID()
+			imgui.SameLine()
+			if imgui.TreeNodeExStr(
+				fmt.Sprintf(
+					"%d. %s Automation %d",
+					i + 1, wwise.ClipAutomationTypeName[c.AutoType], i,
+				),
+			) {
+				autoType := int32(c.AutoType)
+				if imgui.ComboStrarr(
+					"Automation Type",
+					&autoType,
+					wwise.ClipAutomationTypeName,
+					int32(len(wwise.ClipAutomationTypeName)),
+				) {
+					c.AutoType = uint32(autoType)
+				}
+
+				imgui.TreePop()
+			}
+		}
+		imgui.TreePop()
+		if delete != nil {
+			delete()
+		}
+	}
+}
+
+func bindDeleteClipAutomation(o *wwise.MusicTrack, i int) func() {
+	return func() { o.RemoveAutomation(i) }
 }
 
 func renderRanSeqCntr(t *bankTab, o *wwise.RanSeqCntr) {
