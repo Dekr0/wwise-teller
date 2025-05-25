bec23ba57805f26ae58df3b1800973e5bb0683fb
Dekr0 <dekr0.dk@protonmail.com>
2025-05-25 12:03:08 -0600

    update build script

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-GOOS=windows GOARCH=amd64 CGO_ENABLED=1 CXX=x86_64-w64-mingw32-g++ CC=x86_64-w64-mingw32-gcc CGO_LDFLAGS='--static -static-libgcc -static-libstdc++' go build -ldflags .
+GOOS=windows GOARCH=amd64 CGO_ENABLED=1 CXX=x86_64-w64-mingw32-g++ CC=x86_64-w64-mingw32-gcc CGO_LDFLAGS='--static -static-libgcc -static-libstdc++' go build .
