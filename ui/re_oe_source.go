2ce4f892755b7edea3b57ab5a957d336d1a967ee
Dekr0 <dekr0.dk@protonmail.com>
2025-05-26 00:03:10 -0600

    Forget to get the referenced of BankSourceData instead of copying the value

--- ui/re_oe_source.go
+++ ui/re_oe_source.go
@@ -11,12 +11,12 @@ import (
 
 func renderBankSourceData(t *bankTab, o *wwise.Sound) {
 	if imgui.TreeNodeExStr("Bank Source Data") {
-		bsd := o.BankSourceData
+		bsd := &o.BankSourceData
 		imgui.Text(fmt.Sprintf("Plugin Type ID: %d", bsd.PluginType()))
 		imgui.Text(fmt.Sprintf("Plugin Company ID: %d", bsd.Company()))
 		imgui.Text("Stream Type: " + wwise.SourceType[bsd.StreamType])
 
-		renderChangeSourceQuery(t, &bsd)
+		renderChangeSourceQuery(t, bsd)
 		imgui.SameLine()
 		renderChangeSourceTable(t)
 
@@ -29,6 +29,7 @@ func renderBankSourceData(t *bankTab, o *wwise.Sound) {
 		imgui.Checkbox("Non cacheable", &nonCacheAble)
 
 		imgui.EndDisabled()
+
 		imgui.TreePop()
 	}
 }
@@ -42,15 +43,16 @@ func renderChangeSourceQuery(t *bankTab, bsd *wwise.BankSourceData) {
 		t.filterRewireQuery()
 	}
 
-	preview := strconv.FormatUint(uint64(bsd.SourceID), 10)
 	imgui.Text("Source ID")
-	if imgui.BeginComboV("##Source ID", preview, 0) {
+
+	var changeSource func() = nil
+	preview := strconv.FormatUint(uint64(bsd.SourceID), 10)
+	if imgui.BeginComboV("##SourceIDCombo", preview, 0) {
 		for _, m := range t.filteredMediaIndexs {
 			selected := bsd.SourceID == m.Sid
 			preview := strconv.FormatUint(uint64(m.Sid), 10)
 			if imgui.SelectableBoolPtr(preview, &selected) {
-				bsd.SourceID = m.Sid
-				bsd.InMemoryMediaSize = m.Size
+				changeSource = bindChangeSource(bsd, m.Sid, m.Size)
 			}
 			if selected {
 				imgui.SetItemDefaultFocus()
@@ -58,8 +60,17 @@ func renderChangeSourceQuery(t *bankTab, bsd *wwise.BankSourceData) {
 		}
 		imgui.EndCombo()
 	}
+	if changeSource != nil {
+		changeSource()
+	}
 
 	imgui.EndChild()
+}
+
+func bindChangeSource(bsd *wwise.BankSourceData, sid, inMemorySize uint32) func() {
+	return func() {
+		bsd.ChangeSource(sid, inMemorySize)
+	}
 }
 
 func renderChangeSourceTable(t *bankTab) {
