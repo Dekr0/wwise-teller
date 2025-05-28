8132399924fd10e1e0cda5e79c5eab1c63577d5c
Dekr0 <dekr0.dk@protonmail.com>
2025-05-28 04:43:15 -0600

    - Remove remove old test

--- wwise/hierarchy_component_test.go
+++ wwise/hierarchy_component_test.go
@@ -23,54 +23,54 @@ func TestFxChunkMetadataEncode(t *testing.T) {
 	}
 }
 
-func TestPropBundleAddFull(t *testing.T) {
-	p := NewPropBundle()
-	for range len(PropLabel_140) {
-		if _, err := p.New(); err != nil {
-			t.Fail()
-		}
-	}
-	if _, err := p.New(); err != nil {
-		t.Fail()
-		return
-	}
-	if len(p.PropValues) != len(PropLabel_140) {
-		t.Fail()
-		return
-	}
-}
+// func TestPropBundleAddFull(t *testing.T) {
+// 	p := NewPropBundle()
+// 	for range len(PropLabel_140) {
+// 		if _, err := p.New(); err != nil {
+// 			t.Fail()
+// 		}
+// 	}
+// 	if _, err := p.New(); err != nil {
+// 		t.Fail()
+// 		return
+// 	}
+// 	if len(p.PropValues) != len(PropLabel_140) {
+// 		t.Fail()
+// 		return
+// 	}
+// }
 
-func TestPropBundleNewExist(t *testing.T) {
-	p := &PropBundle{
-		[]PropValue{
-			{0, []byte{0, 0, 0, 0}},
-			{6, []byte{0, 0, 0, 0}},
-			{73, []byte{0, 0, 0, 0}},
-		},
-	}
-	if _, err := p.New(); err != nil {
-		t.Fail()
-		return
-	}
-	if _, found := p.HasPid(1); !found {
-		t.Fail()
-		return
-	}
-}
+// func TestPropBundleNewExist(t *testing.T) {
+// 	p := &PropBundle{
+// 		[]PropValue{
+// 			{0, []byte{0, 0, 0, 0}},
+// 			{6, []byte{0, 0, 0, 0}},
+// 			{73, []byte{0, 0, 0, 0}},
+// 		},
+// 	}
+// 	if _, err := p.New(); err != nil {
+// 		t.Fail()
+// 		return
+// 	}
+// 	if _, found := p.HasPid(1); !found {
+// 		t.Fail()
+// 		return
+// 	}
+// }
 
-func TestPropBundleNewWithUpdate(t *testing.T) {
-	p := &PropBundle{}
-	if _, err := p.New(); err != nil {
-		t.Fail()
-		return
-	}
-	if _, err := p.New(); err != nil {
-		t.Fail()
-		return
-	}
-	p.UpdatePropF32(6, -96.0)
-	if _, found := p.HasPid(6); !found {
-		t.Fail()
-		return
-	}
-}
+// func TestPropBundleNewWithUpdate(t *testing.T) {
+// 	p := &PropBundle{}
+// 	if _, err := p.New(); err != nil {
+// 		t.Fail()
+// 		return
+// 	}
+// 	if _, err := p.New(); err != nil {
+// 		t.Fail()
+// 		return
+// 	}
+// 	p.UpdatePropF32(6, -96.0)
+// 	if _, found := p.HasPid(6); !found {
+// 		t.Fail()
+// 		return
+// 	}
+// }
