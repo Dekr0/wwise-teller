ad8c20a03b2cb2942904198c85deffc3a5ad7de1
Dekr0 <dekr0.dk@protonmail.com>
2025-06-10 09:51:21 -0600

    - Move FxChunk and FxChunkMetadata to a dedicated file

---
+++ wwise/fx_chunk.go
@@ -0,0 +1,67 @@
+package wwise
+
+import "github.com/Dekr0/wwise-teller/wio"
+
+
+type FxChunk struct {
+	// UniqueNumFx uint8
+	BitsFxByPass uint8
+	FxChunkItems []FxChunkItem
+}
+
+func (f *FxChunk) Encode() []byte {
+	if len(f.FxChunkItems) <= 0 {
+		return []byte{0}
+	}
+	size := f.Size()
+	w := wio.NewWriter(uint64(size))
+	w.AppendByte(uint8(len(f.FxChunkItems)))
+	w.AppendByte(f.BitsFxByPass)
+	for _, i := range f.FxChunkItems {
+		w.Append(i)
+	}
+	return w.BytesAssert(int(size))
+}
+
+func (f *FxChunk) Size() uint32 {
+	if len(f.FxChunkItems) <= 0 {
+		return 1
+	}
+	return uint32(1 + 1 + len(f.FxChunkItems) * SizeOfFxChunk)
+}
+
+const SizeOfFxChunk = 7
+type FxChunkItem struct {
+	UniqueFxIndex uint8 // u8i
+	FxId          uint32 // tid
+	BitIsShareSet uint8 // U8x
+	BitIsRendered uint8 // U8x unused (effects can't render)
+}
+
+type FxChunkMetadata struct {
+	BitIsOverrideParentMetadata uint8
+	// UniqueNumFxMetadata uint8
+	FxMetaDataChunkItems []FxChunkMetadataItem
+}
+
+func (f *FxChunkMetadata) Encode() []byte {
+	size := f.Size()
+	w := wio.NewWriter(uint64(size))
+	w.AppendByte(f.BitIsOverrideParentMetadata)
+	w.AppendByte(uint8(len(f.FxMetaDataChunkItems)))
+	for _, i := range f.FxMetaDataChunkItems {
+		w.Append(i)
+	}
+	return w.BytesAssert(int(size))
+}
+
+func (f *FxChunkMetadata) Size() uint32 {
+	return uint32(1 + 1 + len(f.FxMetaDataChunkItems) * SizeOfFxChunkMetadata)
+}
+
+const SizeOfFxChunkMetadata = 6
+type FxChunkMetadataItem struct {
+	UniqueFxIndex uint8  // u8i
+	FxId          uint32 // tid
+	BitIsShareSet uint8  // U8x
+}
