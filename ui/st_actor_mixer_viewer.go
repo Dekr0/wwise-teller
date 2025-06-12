a0e4e6dbc15436d61437713555796af6c76ed66a
Dekr0 <dekr0.dk@protonmail.com>
2025-06-12 08:25:06 -0600

    - Converted the original `HircFilter` to ActorMixerHircFilter that only   do filtering on actor mixer hierarchy - Converted the original `HircRootFilter` to ActorMixerRootFilter that   only do filtering on actor mixer hierarchy root - Moved MediaIndexFilter to `st_bank_filter.go` - Moved BankTab.CntrStorage to ActorMixerViewer, and it only keeps   track of selection state for a hierarchy that is in actor mixer   hierarchy - Moved BankTab.RanSeqPlayListStorage to ActorMixerViewer - Moved ActiveHirc to ActorMixerViewer (as "ActiveActorMixerHirc"), and   it keeps track of active actor mixer hierarchy instead of all   different types of hierarchy - Made the original `LinearStorage` to keep track of selection state of   all actor mixer hierarchy only. Moved it into ActiveActorMixerHirc - BankTab has ActorMixerViewer and MusicHircView to keep tracks of the   UI state for actor mixer hierarchy and music hierarchy (separately) - Calculate the total number hierarchies for different types before   doing the initialization / allocation for filter. - Remove Node struct

---
+++ ui/st_actor_mixer_viewer.go
@@ -0,0 +1,108 @@
+package ui
+
+import (
+	"slices"
+	"strconv"
+	"github.com/AllenDang/cimgui-go/imgui"
+	"github.com/Dekr0/wwise-teller/wwise"
+	"github.com/lithammer/fuzzysearch/fuzzy"
+)
+
+type ActorMixerViewer struct {
+	ActorMixerHircFilter ActorMixerHircFilter
+	ActorMixerRootFilter ActorMixerRootFilter
+	ActiveActorMixerHirc wwise.HircObj
+
+	// Storage
+	LinearStorage         *imgui.SelectionBasicStorage
+	CntrStorage           *imgui.SelectionBasicStorage
+	RanSeqPlaylistStorage *imgui.SelectionBasicStorage
+}
+
+type ActorMixerHircFilter struct {
+	Id                uint32
+	Sid               uint32
+	Type              wwise.HircType
+	ActorMixerHircs []wwise.HircObj
+}
+
+func (f *ActorMixerHircFilter) Filter(objs []wwise.HircObj) {
+	curr := 0 
+	prev := len(f.ActorMixerHircs)
+	for _, obj := range objs {
+		if !wwise.ActorMixerHircType(obj) {
+			continue
+		}
+		if f.Type > 0 && f.Type != obj.HircType() {
+			continue
+		}
+		sound := obj.HircType() == wwise.HircTypeSound
+		bySid := f.Type == 0 || f.Type == wwise.HircTypeSound
+		if sound && bySid {
+			sound := obj.(*wwise.Sound)
+			if !fuzzy.Match(
+				strconv.FormatUint(uint64(f.Sid), 10),
+				strconv.FormatUint(uint64(sound.BankSourceData.SourceID), 10),
+			) {
+				continue
+			}
+		}
+		id, err := obj.HircID()
+		if err != nil {
+			continue
+		}
+		if !fuzzy.Match(
+			strconv.FormatUint(uint64(f.Id), 10),
+			strconv.FormatUint(uint64(id), 10),
+		) {
+			continue
+		}
+		if curr < len(f.ActorMixerHircs) {
+			f.ActorMixerHircs[curr] = obj
+		} else {
+			f.ActorMixerHircs = append(f.ActorMixerHircs, obj)
+		}
+		curr += 1
+	}
+	if curr < prev {
+		f.ActorMixerHircs = slices.Delete(f.ActorMixerHircs, curr, prev)
+	}
+}
+
+type ActorMixerRootFilter struct {
+	Id                uint32
+	Type              wwise.HircType
+	ActorMixerRoots []wwise.HircObj
+}
+
+func (f *ActorMixerRootFilter) Filter(objs []wwise.HircObj) {
+	curr := 0
+	prev := len(f.ActorMixerRoots)
+	for _, obj := range objs {
+		if !wwise.ContainerActorMixerHircType(obj) {
+			continue
+		}
+		if f.Type > 0 && f.Type != obj.HircType() {
+			continue
+		}
+		id, err := obj.HircID()
+		if err != nil {
+			continue
+		}
+		if !fuzzy.Match(
+			strconv.FormatUint(uint64(f.Id), 10),
+			strconv.FormatUint(uint64(id), 10),
+		) {
+			continue
+		}
+		if curr < len(f.ActorMixerRoots) {
+			f.ActorMixerRoots[curr] = obj
+		} else {
+			f.ActorMixerRoots = append(f.ActorMixerRoots, obj)
+		}
+		curr += 1
+	}
+	if curr < prev {
+		f.ActorMixerRoots = slices.Delete(f.ActorMixerRoots, curr, prev)
+	}
+}
