444ea84d4ba5da49984039b4f960182ab8570a7e
Dekr0 <dekr0.dk@protonmail.com>
2025-06-16 19:10:43 -0600

    - Prototyping append new hierarchy object into the sound bank (Step 1):   adding new sound objects and wire up the newly added audio source, and   attach it to a random / sequence container.

--- integration_test.go
+++ integration_test.go
@@ -5,7 +5,9 @@ import (
 	// "slices"
 	// "context"
 	"context"
+	"fmt"
 	"os"
+	"slices"
 	"testing"
 	"time"
 
@@ -24,7 +26,7 @@ func TestAppendAudio(t *testing.T) {
 	}
 	hirc := bnk.HIRC()
 
-	audioData, err := os.ReadFile("./tests/wems/reflection_close_desert.wem")
+	audioData, err := os.ReadFile("./tests/wems/reflection_close_desert_00.wem.wem")
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -40,10 +42,100 @@ func TestAppendAudio(t *testing.T) {
 			}
 		case *wwise.RanSeqCntr:
 			if s.Id == 274049716 {
-				s.BaseParam.PropBundle.SetPropByIdxF32(wwise.PropTypeMakeUpGain, 6.4)
+				s.BaseParam.PropBundle.SetPropByIdxF32(wwise.PropTypeMakeUpGain, 12.8)
 			}
 		}
 	}
+
+	ctx, cancel = context.WithTimeout(context.Background(), time.Second * 2)
+	defer cancel()
+	data, err := bnk.Encode(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	helldivers.GenHelldiversPatchStable(data, bnk.META().B, "./tests/patch")
+}
+
+func TestAppendAudioAndSound(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second * 2)
+	defer cancel()
+	bnk, err := parser.ParseBank("./tests/st_bnks/wep_cr1_adjudicator.st_bnk", ctx)
+	if err != nil {
+		cancel()
+		t.Fatal(err)
+	}
+	hirc := bnk.HIRC()
+
+	audioDataDesert, err := os.ReadFile("./tests/wems/reflection_close_desert_00.wem")
+	if err != nil {
+		t.Fatal(err)
+	}
+	bnk.AppendAudio(audioDataDesert, 26007159)
+	for i := range hirc.HircObjs {
+		switch s := hirc.HircObjs[i].(type) {
+		case *wwise.Sound:
+			if s.BaseParam.DirectParentId == 274049716 {
+				s.BankSourceData.PluginID = wwise.VORBIS
+				s.BankSourceData.SourceID = 26007159
+				s.BankSourceData.InMemoryMediaSize = uint32(len(audioDataDesert))
+			}
+		case *wwise.RanSeqCntr:
+			if s.Id == 274049716 {
+				fmt.Println("274049716")
+				s.BaseParam.PropBundle.SetPropByIdxF32(wwise.PropTypeMakeUpGain, 18.0)
+			}
+			if s.Id == 435636362 {
+				fmt.Println("435636362")
+				s.BaseParam.PropBundle.AddBaseProp()	
+				s.BaseParam.PropBundle.ChangeBaseProp(0, wwise.PropTypeMakeUpGain)
+				s.BaseParam.PropBundle.SetPropByIdxF32(wwise.PropTypeMakeUpGain, -96.0)
+			}
+			if s.Id == 98920475 {
+				fmt.Println("98920475")
+				s.BaseParam.PropBundle.SetPropByIdxF32(wwise.PropTypeMakeUpGain, -96.0)
+			}
+		}
+	}
+
+	idx := slices.IndexFunc(hirc.HircObjs, func(h wwise.HircObj) bool {
+		id, err := h.HircID()
+		if err != nil {
+			return false
+		}
+		return id == 80388110
+	})
+	ref := hirc.HircObjs[idx].(*wwise.Sound)
+
+	audioDataUrban0, err := os.ReadFile("./tests/wems/reflection_close_urban_00.wem")
+	bnk.AppendAudio(audioDataUrban0, 568558003)
+	soundA := wwise.Sound{
+		Id: 415678546,
+		BankSourceData: wwise.BankSourceData{
+			PluginID: wwise.VORBIS,
+			StreamType: wwise.STREAM_TYPE_BNK,
+			SourceID: 568558003,
+			InMemoryMediaSize: uint32(len(audioDataUrban0)),
+			SourceBits: 0,
+		},
+		BaseParam: ref.BaseParam.Clone(false),
+	}
+
+	audioDataUrban1, err := os.ReadFile("./tests/wems/reflection_close_urban_01.wem")
+	bnk.AppendAudio(audioDataUrban1, 107266693)
+	soundB := wwise.Sound{
+		Id: 234198813,
+		BankSourceData: wwise.BankSourceData{
+			PluginID: wwise.VORBIS,
+			StreamType: wwise.STREAM_TYPE_BNK,
+			SourceID: 107266693,
+			InMemoryMediaSize: uint32(len(audioDataUrban1)),
+			SourceBits: 0,
+		},
+		BaseParam: ref.BaseParam.Clone(false),
+	}
+
+	hirc.AppendNewSoundToRanSeqContainer(&soundA, 274049716)
+	hirc.AppendNewSoundToRanSeqContainer(&soundB, 274049716)
 
 	ctx, cancel = context.WithTimeout(context.Background(), time.Second * 2)
 	defer cancel()
