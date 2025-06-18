ad4435d2385503c829240035c3e751e321ff6a08
Dekr0 <dekr0.dk@protonmail.com>
2025-06-17 19:34:26 -0600

    - Split the original test into two parts: one for conversion listing   creation, another one for calling WwiseConsole to perform conversion - Convert all file path to absolute path before passing them as   parameters

--- waapi/conversion_test.go
+++ waapi/conversion_test.go
@@ -2,27 +2,73 @@ package waapi
 
 import (
 	"context"
+	"os"
+	"path/filepath"
 	"testing"
 )
 
-func TestWaveToWem(t *testing.T) {
+func TestCreateConversionList(t *testing.T) {
 	in := []string{
 		"../tests/wavs/ak74/core_01.wav",
 		"../tests/wavs/ak74/core_02.wav",
 		"../tests/wavs/ak74/core_03.wav",
 		"../tests/wavs/ak74/core_04.wav",
 	}
+	var err error
+	for i := range in {
+		in[i], err = filepath.Abs(in[i])
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
 	out := make([]string, len(in))
 	ctx := context.Background()
-	wsource, err := CreateConversionList( ctx, in, out, "Vorbis Quality High"); 
+	if err := InitTmp(); err != nil {
+		t.Fatal(err)
+	}
+	wsource, err := CreateConversionList(ctx, in, out, "Vorbis Quality High", true); 
 	if err != nil {
 		t.Fatal(err)
 	}
-	if err := WwiseConversion(
-		ctx,
-		wsource,
-		"../AudioConversionProject/AudioConversionProject.wproj",
-	); err != nil {
+	content, err := os.ReadFile(wsource)
+	if err != nil {
 		t.Fatal(err)
 	}
+	t.Log(string(content))
+	CleanTmp()
+}
+
+func TestWwiseConversion(t *testing.T) {
+	in := []string{
+		"../tests/wavs/ak74/core_01.wav",
+		"../tests/wavs/ak74/core_02.wav",
+		"../tests/wavs/ak74/core_03.wav",
+		"../tests/wavs/ak74/core_04.wav",
+	}
+	var err error
+	for i := range in {
+		in[i], err = filepath.Abs(in[i])
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	out := make([]string, len(in))
+	ctx := context.Background()
+	if err := InitTmp(); err != nil {
+		t.Fatal(err)
+	}
+	wsource, err := CreateConversionList(ctx, in, out, "Vorbis Quality High", true); 
+	if err != nil {
+		t.Fatal(err)
+	}
+	proj, err := filepath.Abs("WwiseTeller") 
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := WwiseConversion(ctx, wsource, proj); err != nil {
+		t.Fatal(err)
+	}
+	CleanTmp()
 }
