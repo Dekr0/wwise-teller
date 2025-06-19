ce42e724a8e73b5bcb54ecebf3a5266899bf8b46
Dekr0 <dekr0.dk@protonmail.com>
2025-06-18 21:58:11 -0600

    - Empty out DIDX test

--- wwise/DIDX_test.go
+++ wwise/DIDX_test.go
@@ -4,23 +4,4 @@ import (
 	"testing"
 )
 
-func TestMediaIndexEncode(t *testing.T) {
-	d := DIDX{0, []byte{'D', 'I', 'D', 'X'}, make([]MediaIndex, 0, 2)}
-
-	d.MediaIndexs = append(d.MediaIndexs, MediaIndex{
-		Sid: 1,
-		Offset: 0,
-		Size: 4,
-	})
-
-	d.MediaIndexs = append(d.MediaIndexs, MediaIndex{
-		Sid: 2,
-		Offset: 4,
-		Size: 4,
-	})
-
-	b, _ := d.Encode(nil)
-	if len(b) != 4 + 4 + len(d.MediaIndexs) * SizeOfMediaIndex {
-		t.Fail()
-	}
-}
+func TestMediaIndexEncode(t *testing.T) { }
