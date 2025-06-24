ada04281869ab64b43d756ec1e2d64c38d62ace0
Dekr0 <dekr0.dk@protonmail.com>
2025-06-24 10:07:20 -0600

    - Version 0 of audio source rewiring for Sound objects. Ready to test on   Windows

--- automation/automation.go
+++ automation/automation.go
@@ -3,6 +3,7 @@ package automation
 import (
 	"context"
 	"encoding/csv"
+	"errors"
 	"fmt"
 	"io"
 	"log/slog"
@@ -11,13 +12,101 @@ import (
 	"strconv"
 	"strings"
 
+	"github.com/Dekr0/wwise-teller/db"
+	"github.com/Dekr0/wwise-teller/db/id"
+	"github.com/Dekr0/wwise-teller/utils"
+	"github.com/Dekr0/wwise-teller/waapi"
 	"github.com/Dekr0/wwise-teller/wwise"
+	"github.com/cenkalti/backoff"
+)
+
+func TrySid(ctx context.Context, q *id.Queries) (uint32, error) {
+	b := backoff.WithContext(backoff.WithMaxRetries(backoff.NewExponentialBackOff(), 16), ctx)
+	var sid uint32 = 0
+	if err := backoff.Retry(func() error {
+		var err error
+		sid, err = utils.ShortID()
+		if err != nil {
+			slog.Error("Failed to generate 32 bit unsigned integer ID", "error", err)
+			return err
+		}
+		count, err := q.SourceId(ctx, int64(sid))
+		if err != nil {
+			slog.Error("Failed to query source ID from database", "error", err)
+			return err
+		}
+		if count > 0 {
+			err := fmt.Errorf("Source ID %d already exists.", sid)
+			slog.Error(err.Error())
+			return err
+		}
+		return nil
+	}, b); err != nil {
+		return 0, err
+	}
+	if sid == 0 {
+		return 0, errors.New("Source ID uses invalid value of 0.")
+	}
+	if err := q.InsertSource(ctx, int64(sid)); err != nil {
+		return 0, err
+	}
+	return sid, nil
+}
+
+func TryHid(ctx context.Context, q *id.Queries) (uint32, error) {
+	b := backoff.WithContext(backoff.WithMaxRetries(backoff.NewExponentialBackOff(), 16), ctx)
+	var hid uint32 = 0
+	if err := backoff.Retry(func() error {
+		var err error
+		hid, err = utils.ShortID()
+		if err != nil {
+			slog.Error("Failed to generate 32 bit unsigned integer ID", "error", err)
+			return err
+		}
+		count, err := q.HierarchyId(ctx, int64(hid))
+		if err != nil {
+			slog.Error("Failed to query source ID from database", "error", err)
+			return err
+		}
+		if count > 0 {
+			err := fmt.Errorf("Source ID %d already exists.", hid)
+			slog.Error(err.Error())
+			return err
+		}
+		return nil
+	}, b); err != nil {
+		return 0, err
+	}
+	if hid == 0 {
+		return 0, errors.New("Source ID uses invalid value of 0.")
+	}
+	if err := q.InsertHierarchy(ctx, int64(hid)); err != nil {
+		return 0, err
+	}
+	return hid, nil
+}
+
+type SourceImportType uint8;
+
+type CSVHeader struct {
+	Workspace string
+	Output    string
+	Type      SourceImportType
+}
+
+const (
+	SourceImportTypeSound SourceImportType = 0
+	SourceImportTypeCntr  SourceImportType = 1
 )
 
 func RewireSoundsWithNewSourcesCSV(
-	ctx context.Context,
-	h *wwise.HIRC,
+	ctx         context.Context,
+	h          *wwise.HIRC,
+	d          *wwise.DIDX,
 	mappingFile string,
+	conversion  string,
+	project     string,
+	dry         bool,
 ) error {
 	f, err := os.Open(mappingFile)
 	if err != nil {
@@ -25,47 +114,198 @@ func RewireSoundsWithNewSourcesCSV(
 	}
 
 	reader := csv.NewReader(f)
-	header, err := reader.Read()
+	iniDir := filepath.Base(mappingFile)
+	header := CSVHeader{
+		Workspace: iniDir,
+		Output: iniDir,
+		Type: 0,
+	}
+	if err = ParseCSVHeader(&header, reader); err != nil {
+		return err
+	}
+
+	wavsMapSound := make(map[string][]*wwise.Sound, 32)
+	err = ParseSoundMapping(reader, &header, h, wavsMapSound)
 	if err != nil {
-		return fmt.Errorf("Failed to obtain workspace directory: %w", err)
+		return err
 	}
-	if strings.Compare(header[0], "workspace") != 0 {
-		return fmt.Errorf("Expecting Row 1, Column 1 to be workspace")
+
+	wemsMapSound := make(map[string][]*wwise.Sound, len(wavsMapSound))
+	wsource, err := waapi.CreateConversionList(ctx, wavsMapSound, wemsMapSound, conversion, dry)
+	if len(wavsMapSound) != len(wemsMapSound) {
+		panic("Panic Trap")
 	}
-	workspace := filepath.Dir(mappingFile)
-	if len(header) >= 2 {
-		overwrite := header[1]
-		// Relative path
-		if !filepath.IsAbs(overwrite) {
-			overwrite = filepath.Join(workspace, overwrite)
+	if err != nil {
+		return err
+	}
+
+	if dry {
+		for wem, sounds := range wemsMapSound {
+			fmt.Println(wem)
+			for _, s := range sounds {
+				fmt.Println(s.Id)
+			}
+			fmt.Println()
 		}
-		_, err := os.Lstat(overwrite)
+		return nil
+	}
+
+	if err := waapi.WwiseConversion(ctx, wsource, project); err != nil {
+		return err
+	}
+
+	wemsMapAudioData := make(map[string][]byte, len(wemsMapSound))
+	errorWems := make([]string, 0, len(wemsMapSound))
+	for wem := range wemsMapSound {
+		if _, in := wemsMapAudioData[wem]; in {
+			panic("Panic Trap")
+		}
+		wemsMapAudioData[wem], err = os.ReadFile(wem)
 		if err != nil {
-			if os.IsNotExist(err) {
-				slog.Info(fmt.Sprintf("%s does not exist. Creating this workspace", overwrite))
-				if err := os.MkdirAll(overwrite, 0777); err != nil {
-					slog.Error(fmt.Sprintf("Failed to create workspace %s", overwrite), "error", err)
-					slog.Info(fmt.Sprintf("Fallback to workspace %s", workspace))
-				} else {
-					workspace = overwrite
-				}
-			} else {
-				slog.Error(fmt.Sprintf("Failed to obtain information about workspace %s", workspace), "error", err)
-				slog.Info(fmt.Sprintf("Fallback to workspace %s", workspace))
+			slog.Error(fmt.Sprintf("Failed to read audio data from %s", wem))
+			slog.Error(fmt.Sprintf("Discard all rewiring related to file %s", wem))
+			errorWems = append(errorWems, wem)
+		}
+	}
+	for _, errorWem := range errorWems {
+		delete(wemsMapSound, errorWem)
+	}
+
+	q, closeDb, commit, txRollback, err := db.CreateDefaultConnWithTxQuery(ctx)
+	if err != nil {
+		return err
+	}
+	defer closeDb()
+
+	wemsMapMediaIndexs := make(map[string]wwise.MediaIndex, len(wemsMapSound))
+	for wem := range wemsMapSound {
+		sid, err := TrySid(ctx, q)
+		if err != nil {
+			txRollback()
+			return fmt.Errorf("Failed to allocate a new source ID: %w", err)
+		}
+		if _, in := wemsMapMediaIndexs[wem]; in { panic("Panic Trap") }
+		if audioData, in := wemsMapAudioData[wem]; !in { 
+			panic("Panic Trap") 
+		} else {
+			wemsMapMediaIndexs[wem] = wwise.MediaIndex{Sid: sid, Size: uint32(len(audioData))}
+		}
+	}
+
+	if err := commit(); err != nil {
+		slog.Error(err.Error())
+		txRollback()
+		return err
+	}
+
+	for wem, m := range wemsMapMediaIndexs {
+		if audioData, in := wemsMapAudioData[wem]; !in {
+			panic("Panic Trap")
+		} else {
+			if err := d.Append(m.Sid, uint32(len(audioData))); err != nil {
+				// Database is out of sync
+				panic("Panic Trap")
 			}
 		}
 	}
 
-	mapping := make(map[string][]*wwise.Sound, 32)
+	for wem, sounds := range wemsMapSound {
+		m, in := wemsMapMediaIndexs[wem]
+		if !in { panic("Panic Trap") }
+		for _, sound := range sounds {
+			sound.BankSourceData.SourceID = m.Sid
+			sound.BankSourceData.InMemoryMediaSize = m.Size
+		}
+	}
+
+	return nil
+}
+
+func ParseCSVHeader(header *CSVHeader, reader *csv.Reader) error {
+	workspace_line, err := reader.Read()
+	if err != nil {
+		return fmt.Errorf("Failed to obtain workspace directory: %w", err)
+	}
+	if strings.Compare(workspace_line[0], "workspace") != 0 {
+		return fmt.Errorf("Expecting Row 1, Column 1 to be workspace")
+	}
+	// If workspace value is provided, overwrite default
+	if len(workspace_line) >= 2 && len(workspace_line[1]) > 0 {
+		overwrite := workspace_line[1]
+		if !filepath.IsAbs(overwrite) {
+			return fmt.Errorf("Workspace path %s is not in full path format", overwrite)
+		}
+		_, err := os.Lstat(overwrite)
+		if err != nil {
+			if os.IsNotExist(err) {
+				return fmt.Errorf("Workspace %s does not exist", overwrite)
+			}
+			return fmt.Errorf("Failed to obtain information of workspace path %s: %w", overwrite, err)
+		}
+		header.Workspace = overwrite
+	}
+
+	output_line, err := reader.Read()
+	if err != nil {
+		return fmt.Errorf("Failed to obtain output directory: %w", err)
+	}
+	if strings.Compare(output_line[0], "output") != 0 {
+		return fmt.Errorf("Expecting Row 2, Column 1 to be output")
+	}
+	if len(output_line) >= 2 && len(output_line[1]) > 0 {
+		overwrite := output_line[1]
+		if !filepath.IsAbs(overwrite) {
+			return fmt.Errorf("Output path %s is not in full path format", overwrite)
+		}
+		_, err := os.Lstat(overwrite)
+		if err != nil {
+			if os.IsNotExist(err) {
+				if err := os.MkdirAll(overwrite, 0777); err != nil {
+					slog.Error(fmt.Sprintf("Failed to create output %s", overwrite), "error", err)
+					slog.Info(fmt.Sprintf("Fallback to output %s", header.Output))
+				} else {
+					header.Output = overwrite
+				}
+			} else {
+				slog.Error(fmt.Sprintf("Failed to obtain information about output %s", overwrite), "error", err)
+				slog.Info(fmt.Sprintf("Fallback to output %s", header.Output))
+			}
+		}
+	}
+	return nil
+}
+
+// Assumption
+// Skip if a row has the following error
+// - less than 2 columns
+// - an input is not in wave format
+// - an input does not exist
+// - not an unsigned integer for # of sound IDs specified
+// - provided # of sound IDs is less than # of sound IDs specified
+// - not an unsigned integer for a sound ID
+// It will append .wav extension if no extension is provided
+// It will use workspace to construct full path if a relative path is provided
+// Skip a sound ID if it doesn't exist
+// Merge duplicate input
+func ParseSoundMapping(
+	reader *csv.Reader,
+	header *CSVHeader,
+	h *wwise.HIRC,
+	wavsMapping map[string][]*wwise.Sound,
+) error {
 	rowNum := 2
+	var err   error
+	var row []string
+	var input, ext string
 	for {
-		row, err := reader.Read()
+		row, err = reader.Read()
 		if err != nil {
 			if err == io.EOF {
 				break
 			}
 		}
 
+		// Syntax
 		if len(row) < 2 {
 			slog.Error(fmt.Sprintf("Expecting two columns, filename and # of targeting sound IDs, at row %d", rowNum))
 			rowNum += 1
@@ -73,18 +313,24 @@ func RewireSoundsWithNewSourcesCSV(
 		}
 
 		// Extension
-		input := row[0]
-		ext := filepath.Ext(input)
+		input = row[0]
+		ext = filepath.Ext(input)
+		if ext == "" {
+			ext = ".wav"
+			input += ext
+		}
 		if ext != ".wav" {
 			slog.Error("Wave file is the only supported file format.")
 			rowNum += 1
 			continue
 		}
 
-		// Existence
+		// Full path
 		if !filepath.IsAbs(input) {
-			input = filepath.Join(workspace, input)
+			input = filepath.Join(header.Workspace, input)
 		}
+
+		// Existence
 		_, err = os.Lstat(input)
 		if err != nil {
 			slog.Error(fmt.Sprintf("Failed to obtain info of wave file %s", input), "error", err)
@@ -103,8 +349,8 @@ func RewireSoundsWithNewSourcesCSV(
 			continue
 		}
 
-		if _, in := mapping[row[0]]; !in {
-			// IDing
+		if sounds, in := wavsMapping[input]; !in {
+			// Parse IDs
 			columnNum := 3
 			targets := make([]*wwise.Sound, 0, count)
 			for _, i := range row[2:] {
@@ -123,15 +369,35 @@ func RewireSoundsWithNewSourcesCSV(
 				}
 				columnNum += 1
 			}
-			mapping[row[0]] = targets
+			wavsMapping[input] = targets
 		} else {
-			slog.Error(fmt.Sprintf("Duplicated input file %s at row %d, column 1", row[0], rowNum))
+			slog.Warn(fmt.Sprintf("Duplicated input file %s at row %d, column 1", input, rowNum))
+			// Parse IDs
+			columnNum := 3
+			for _, i := range row[2:] {
+				id, err := strconv.ParseUint(i, 10, 32)
+				if err != nil {
+					slog.Error(fmt.Sprintf("Failed to parse sound ID at column %d", columnNum), "error", err)
+					columnNum += 1
+					continue
+				}
+				if v, ok := h.ActorMixerHirc.Load(uint32(id)); !ok {
+					slog.Error(fmt.Sprintf("ID %d does not have an associated sound object", id))
+				} else {
+					// no duplication check because it will map to the same sid 
+					// at the end
+					sounds = append(sounds, v.(*wwise.Sound))
+				}
+				columnNum += 1
+			}
+			wavsMapping[input] = sounds
 		}
 		rowNum += 1
 	}
 
-	if len(mapping) == 0 {
+	if len(wavsMapping) == 0 {
 		return fmt.Errorf("No mapping is provided. Aborted")
 	}
+
 	return nil
 }
--- automation/automation_test.go
+++ automation/automation_test.go
@@ -1,1 +1,146 @@
 package automation
+
+import (
+	"context"
+	"database/sql"
+	"encoding/csv"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/Dekr0/wwise-teller/db/id"
+	"github.com/Dekr0/wwise-teller/parser"
+	"github.com/Dekr0/wwise-teller/waapi"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var TestBankDir string = "../tests/bnks"
+var TestCSVOkDir string = "./tests/csvs/ok"
+var TestCSVCompleteDir string = "./tests/csvs/ok/complete"
+var TestCSVFailDir string = "./tests/csvs/fail"
+
+func TestParseCSVHeaderOk(t *testing.T) {
+	tests, err := os.ReadDir(TestCSVOkDir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	var p string = ""
+	var b string = ""
+	for _, test := range tests {
+		p = filepath.Join(TestCSVOkDir, test.Name())
+		f, err := os.Open(p)
+		if err != nil {
+			t.Fatal(err)
+		}
+		b = filepath.Base(p)
+		reader := csv.NewReader(f)
+		header := CSVHeader{Workspace: b, Output: b, Type: 0}
+		if err = ParseCSVHeader(&header, reader); err != nil {
+			t.Fatal(err)
+		}
+	}
+}
+
+func TestParseCSVHeaderFail(t *testing.T) {
+	tests, err := os.ReadDir(TestCSVFailDir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	var p string = ""
+	var b string = ""
+	for _, test := range tests {
+		p = filepath.Join(TestCSVFailDir, test.Name())
+		f, err := os.Open(p)
+		if err != nil {
+			t.Fatal(err)
+		}
+		b = filepath.Base(p)
+		reader := csv.NewReader(f)
+		header := CSVHeader{Workspace: b, Output: b, Type: 0}
+		if err = ParseCSVHeader(&header, reader); err == nil {
+			t.Fatalf("Expecting test case %s to fail", p)
+		} else {
+			t.Log(err)
+		}
+	}
+}
+
+type RewireSoundsWithNewSourcesCSVTest struct {
+	CSV string
+	Bank string
+}
+
+func TestRewireSoundsWithNewSourceCSV(t *testing.T) {
+	waapi.InitTmp()
+	tests := []RewireSoundsWithNewSourcesCSVTest{
+		{
+			"squad_ak74_ar19_01.csv",
+			"wep_ar19_liberator.bnk",
+		},
+		{
+			"squad_ak74_ar19_02.csv",
+			"wep_ar19_liberator.bnk",
+		},
+		{
+			"squad_ak74_ar19_03.csv",
+			"wep_ar19_liberator.bnk",
+		},
+		{
+			"squad_ak74_ar19_04.csv",
+			"wep_ar19_liberator.bnk",
+		},
+		{
+			"squad_ak74_ar19_05.csv",
+			"wep_ar19_liberator.bnk",
+		},
+	}
+	for _, test := range tests {
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second * 2)
+		defer cancel()
+		bnk, err := parser.ParseBank(
+			filepath.Join(TestBankDir, test.Bank),
+			ctx,
+		)
+		if err != nil {
+			t.Fatal(err)
+		}
+		h, d := bnk.HIRC(), bnk.DIDX()
+		if h == nil {
+			t.Fatal("HIRC chunk is nil")
+		}
+		if d == nil {
+			t.Fatal("DIDX chunk is nil")
+		}
+		if err := RewireSoundsWithNewSourcesCSV(
+			ctx,
+			h, 
+			d,
+			filepath.Join(TestCSVCompleteDir, test.CSV),
+			"VORBIS High Quality",
+			"None",
+			true,
+		); err != nil {
+			t.Fatal(err)
+		}
+	}
+	waapi.CleanTmp()
+}
+
+func BenchmarkIDCollisionCheck(b *testing.B) {
+	db, err := sql.Open("sqlite3", "../id_15314")
+	if err != nil {
+		b.Fatal(err)
+	}
+	q := id.New(db)
+	ctx := context.Background()
+	var in uint64
+	for b.Loop() {
+		_, err := TrySid(ctx, q)
+		if err != nil {
+			in += 1
+		}
+	}
+	b.Logf("Collision: %d.\n", in)
+}
