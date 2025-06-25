5ba6b68106ccb996cd166729b11c9b18eabe0da7
Dekr0 <dekr0.dk@protonmail.com>
2025-06-25 17:19:28 -0600

    Expose integration type

---
+++ integration/lib.go
@@ -0,0 +1,10 @@
+package integration
+
+type IntegrationType uint8
+
+const (
+	IntegrationTypeNone        IntegrationType = 0
+	IntegrationTypeDefault     IntegrationType = 1
+	IntegrationTypeHelldivers2 IntegrationType = 2
+)
+
