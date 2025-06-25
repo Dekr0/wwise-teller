348ddefbce46db54a6302f002de403ebf72eb576
Dekr0 <dekr0.dk@protonmail.com>
2025-06-25 17:14:33 -0600

    Add gloabl error value for missing HIRC chunk

--- wwise/bank.go
+++ wwise/bank.go
@@ -14,6 +14,7 @@ import (
 
 var BankVersion = -1
 
+var NoHIRC = errors.New("This sound bank does not have HIRC chunk.")
 var NoDIDX = errors.New("This sound bank does not have DIDX chunk.")
 var NoDATA = errors.New("This sound bank does not have DATA chunk.")
 
