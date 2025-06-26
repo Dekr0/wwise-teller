618da5db2b1a253fb2e9750cc23208dfb3307844
Dekr0 <dekr0.dk@protonmail.com>
2025-06-26 11:46:26 -0600

    - Move id generation to db package

--- db/lib.go
+++ db/lib.go
@@ -3,11 +3,14 @@ package db
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"fmt"
 	"log/slog"
 	"os"
 
 	"github.com/Dekr0/wwise-teller/db/id"
+	"github.com/Dekr0/wwise-teller/utils"
+	"github.com/cenkalti/backoff"
 
 	_ "github.com/ncruces/go-sqlite3/driver"
 	_ "github.com/ncruces/go-sqlite3/embed"
@@ -69,4 +72,70 @@ func CreateDefaultConnWithTxQuery(ctx context.Context) (*id.Queries, func(), fun
 		}
 	}
 	return id.New(db).WithTx(tx), closeDb, commit, rollback, nil
+}
+
+func TrySid(ctx context.Context, q *id.Queries) (uint32, error) {
+	b := backoff.WithContext(backoff.WithMaxRetries(backoff.NewExponentialBackOff(), 16), ctx)
+	var sid uint32 = 0
+	if err := backoff.Retry(func() error {
+		var err error
+		sid, err = utils.ShortID()
+		if err != nil {
+			slog.Error("Failed to generate 32 bit unsigned integer ID", "error", err)
+			return err
+		}
+		count, err := q.SourceId(ctx, int64(sid))
+		if err != nil {
+			slog.Error("Failed to query source ID from database", "error", err)
+			return err
+		}
+		if count > 0 {
+			err := fmt.Errorf("Source ID %d already exists.", sid)
+			slog.Error(err.Error())
+			return err
+		}
+		return nil
+	}, b); err != nil {
+		return 0, err
+	}
+	if sid == 0 {
+		return 0, errors.New("Source ID uses invalid value of 0.")
+	}
+	if err := q.InsertSource(ctx, int64(sid)); err != nil {
+		return 0, err
+	}
+	return sid, nil
+}
+
+func TryHid(ctx context.Context, q *id.Queries) (uint32, error) {
+	b := backoff.WithContext(backoff.WithMaxRetries(backoff.NewExponentialBackOff(), 16), ctx)
+	var hid uint32 = 0
+	if err := backoff.Retry(func() error {
+		var err error
+		hid, err = utils.ShortID()
+		if err != nil {
+			slog.Error("Failed to generate 32 bit unsigned integer ID", "error", err)
+			return err
+		}
+		count, err := q.HierarchyId(ctx, int64(hid))
+		if err != nil {
+			slog.Error("Failed to query source ID from database", "error", err)
+			return err
+		}
+		if count > 0 {
+			err := fmt.Errorf("Source ID %d already exists.", hid)
+			slog.Error(err.Error())
+			return err
+		}
+		return nil
+	}, b); err != nil {
+		return 0, err
+	}
+	if hid == 0 {
+		return 0, errors.New("Source ID uses invalid value of 0.")
+	}
+	if err := q.InsertHierarchy(ctx, int64(hid)); err != nil {
+		return 0, err
+	}
+	return hid, nil
 }
