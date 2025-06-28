ae26ed7f4a23959a953d7dc0ab18cf6d27df3ffa
Dekr0 <dekr0.dk@protonmail.com>
2025-06-28 00:54:27 -0600

    - function for checking and getting Wwise conversion project using   environmental variables - A version of CreateConversionListInOrder that accept an array to store   wem file paths in the order the script provide. - Default error variable / value when no Wwise conversion project is   found.

--- waapi/conversion.go
+++ waapi/conversion.go
@@ -26,6 +26,7 @@ const (
 )
 
 var StagingCollision = errors.New("Staging folder's name collision. Please remove all content in the .cache folder and try again.")
+var NoConversionProj = errors.New("WWISETELLER_WPROJ enviromental variable (used for locating Wwise Project to automate conversion) is not set.")
 
 var Tmp string = ""
 
@@ -55,6 +56,24 @@ func InitTmp() error {
 
 func CleanTmp() error {
 	return os.RemoveAll(Tmp)
+}
+
+func GetProject() (string, error) {
+	proj := os.Getenv("WWISETELLER_WPROJ")
+	if proj == "" {
+		return "", NoConversionProj
+	}
+	if !filepath.IsAbs(proj) {
+		return "", fmt.Errorf("File path of Wwise project %s is not in aboslute path.", proj)
+	}
+	stat, err := os.Lstat(proj)
+	if err != nil {
+		return "", err
+	}
+	if stat.IsDir() {
+		return "", fmt.Errorf("File path of Wwise project %s is a directory.", proj)
+	}
+	return proj, nil
 }
 
 // Assume there's no duplicate wave files. 
@@ -123,6 +142,85 @@ func CreateConversionList[T any](
 				panic("Panic Trap")
 			}
 			wemsMap[dest] = v
+			suffixing[basename] += 1
+		}
+		i += 1
+	}
+
+	x, err := xml.MarshalIndent(&list, "", "    ")
+	if err != nil {
+		return "", err
+	}
+
+	wsource := filepath.Join(staging, "external_sources.wsources")
+	if !dry {
+		if err := os.WriteFile(wsource, []byte(xml.Header + string(x)), 0777); err != nil {
+			return "", err
+		}
+	} else {
+		fmt.Println(xml.Header + string(x))
+	}
+
+	return wsource, nil
+}
+
+func CreateConversionListInOrder(
+	ctx context.Context,
+	wavsMap map[string]uint8,
+	wems []string,
+	conversion string,
+	dry bool,
+) (string, error) {
+	if !dry && runtime.GOOS != "windows" {
+		return "", fmt.Errorf("Wwise External Sources Conversion is only available on Windows")
+	}
+	var err error
+
+	staging := filepath.Join(Tmp, uuid.New().String())
+	_, err = os.Lstat(staging)
+	if err != nil {
+		if !os.IsNotExist(err) {
+			return "", err
+		}
+		err := os.Mkdir(staging, 0777)
+		if err != nil {
+			return "", err
+		}
+	} else {
+		return "", StagingCollision
+	}
+	destF := filepath.Join(staging, "Windows")
+
+	list := ExternalSourcesList{
+		SchemaVersion: 1,
+		Root: staging,
+		Sources: make([]ExternalSource, len(wavsMap)),
+	}
+
+	suffixing := make(map[string]uint8)
+	basename := ""
+	i := 0
+	for wav, idx := range wavsMap {
+		basename = strings.Split(filepath.Base(wav), ".")[0]
+		if suffix, in := suffixing[basename]; !in {
+			suffixing[basename] = 0
+			basename = fmt.Sprintf("%s.wem", basename)
+			dest := filepath.Join(destF, basename)
+			list.Sources[i] = ExternalSource{
+				Path: wav,
+				Conversion: &conversion,
+				Destination: basename,
+			}
+			wems[idx] = dest
+		} else {
+			basename = fmt.Sprintf("%s_%d.wem", basename, suffix)
+			dest := filepath.Join(destF, basename)
+			list.Sources[i] = ExternalSource{
+				Path: wav,
+				Conversion: &conversion,
+				Destination: basename,
+			}
+			wems[idx] = dest
 			suffixing[basename] += 1
 		}
 		i += 1
