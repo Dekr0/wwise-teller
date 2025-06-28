451bc573233c14254c63e7d8f8ab0f39354682d8
Dekr0 <dekr0.dk@protonmail.com>
2025-06-28 14:00:30 -0600

    Construction of Master Mixer Hierarchy

--- wwise/tree.go
+++ wwise/tree.go
@@ -1,6 +1,10 @@
 package wwise
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+	"sort"
+)
 
 type ActorMixerHircNode struct {
 	Obj   HircObj
@@ -15,9 +19,19 @@ type MusicHircNode struct {
 	Leafs []MusicHircNode
 }
 
+type BusHircNode struct {
+	Obj      HircObj
+	Root     uint32 // OverrideBusId
+	LeafsIdx []uint32
+	Leafs    map[uint32]*BusHircNode
+}
+
 func (h *HIRC) BuildTree() {
+	// This should be able to allocated in a deterministic manner because the 
+	// exact # can be accumulative during the parsing phase and post modification phase
 	h.ActorMixerRoots = []ActorMixerHircNode{}
 	h.MusicHircRoots = []MusicHircNode{}
+
 	var o HircObj
 	l := len(h.HircObjs)
 	for i := range h.HircObjs {
@@ -42,6 +56,86 @@ func (h *HIRC) BuildTree() {
 				h.WalkMusicHirciRoot(&node)
 				h.MusicHircRoots = append(h.MusicHircRoots, node)
 			}
+		}
+	}
+
+	// Memory allocation most likely scatter all over the place
+	busNodes := make(map[uint32]*BusHircNode)
+	// This should be able to allocated in a deterministic manner because the 
+	// exact # can be accumulative during the parsing phase and post modification phase
+	h.BusRoots = []BusHircNode{}
+	// First pass, obtain all nodes. Since bus hierarchy object does not keep 
+	// track of leaf buses with a list of IDs, there's chances that a leaf node 
+	// is allocated first but parent node is yet allocated.
+	for _, o := range h.HircObjs {
+		if !BusHircType(o) {
+			continue
+		}
+		switch bus := o.(type) {
+		case *Bus:
+			node := &BusHircNode{
+				bus, bus.OverrideBusId, []uint32{}, make(map[uint32]*BusHircNode),
+			}
+			if _, in := busNodes[bus.Id]; in {
+				panic("Panic Trap")
+			}
+			busNodes[bus.Id] = node
+			if bus.OverrideBusId == 0 {
+				h.BusRoots = append(h.BusRoots, *node)
+			}
+		case *AuxBus:
+			node := &BusHircNode{
+				bus, bus.OverrideBusId, []uint32{}, make(map[uint32]*BusHircNode),
+			}
+			if _, in := busNodes[bus.Id]; in {
+				panic("Panic Trap")
+			}
+			busNodes[bus.Id] = node
+			if bus.OverrideBusId == 0 {
+				h.BusRoots = append(h.BusRoots, *node)
+			}
+		}
+	}
+	// Second pass, construct tree
+	for i, o := range h.HircObjs {
+		if !BusHircType(o) {
+			continue
+		}
+
+		id, err := o.HircID()
+		if err != nil {
+			panic("Panic Trap")
+		}
+
+		node, in := busNodes[id];
+		if !in {
+			panic("Panic Trap")
+		}
+		if node.Root == 0 {
+			continue
+		}
+
+		root, in := busNodes[node.Root]; 
+		if !in {
+			panic("Panic Trap")
+		} 
+
+		if _, in := root.Leafs[uint32(i)]; in {
+			panic("Panic Trap")
+		}
+		root.Leafs[uint32(i)] = node
+
+		insertIdx, in := sort.Find(len(root.LeafsIdx), func(j int) int {
+			if root.LeafsIdx[j] == uint32(i) {
+				panic("Panic Trap")
+			}
+			if root.LeafsIdx[j] > uint32(i) {
+				return 1
+			}
+			return -1
+		})
+		if !in {
+			root.LeafsIdx = slices.Insert(root.LeafsIdx, insertIdx, uint32(i))
 		}
 	}
 }
