06b4dcc96db1314cc7ae8137a3a20543a05868a6
Dekr0 <dekr0.dk@protonmail.com>
2025-06-29 14:02:52 -0600

    - Global context / state (Config, Event Loop, and Modal queue)

--- ui/context.go
+++ ui/context.go
@@ -1,1 +1,18 @@
 package ui
+
+import (
+	"github.com/Dekr0/wwise-teller/config"
+	"github.com/Dekr0/wwise-teller/ui/async"
+)
+
+type Context struct {
+	Loop      async.EventLoop
+	ModalQ    ModalQ
+	Config    config.Config
+}
+
+var GlobalCtx Context = Context{
+	async.NewEventLoop(),
+	NewModalQ(),
+	config.Config{},
+}
