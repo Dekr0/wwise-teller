63f7a651f52bfe7da49ec2358ba48d77d21c94f0
Dekr0 <dekr0.dk@protonmail.com>
2025-06-29 14:04:32 -0600

    Make event loop, modal queue, config to be global states

--- ui/re_config.go
+++ ui/re_config.go
@@ -5,21 +5,20 @@ import (
 	"log/slog"
 
 	"github.com/AllenDang/cimgui-go/imgui"
-	"github.com/Dekr0/wwise-teller/config"
 )
 
-func configModalFunc(modalQ *ModalQ,conf *config.Config) (func(), *bool) {
+func configModalFunc() (func(), *bool) {
 	done := false
 	return func() {
 		imgui.Checkbox("Disable All Guard Rails", &ModifiyEverything)
-		imgui.Text(fmt.Sprintf("Home: %s", conf.Home))
+		imgui.Text(fmt.Sprintf("Home: %s", GlobalCtx.Config.Home))
 		imgui.SameLine()
 		if imgui.ArrowButton("SetHome", imgui.DirRight) {
-			pushSetHomeModal(modalQ, conf)
+			pushSetHomeModal()
 		}
 
 		if imgui.Button("Save") {
-			if err := conf.Save(); err != nil {
+			if err := GlobalCtx.Config.Save(); err != nil {
 				slog.Error("Failed to save configuration")
 			}
 		}
