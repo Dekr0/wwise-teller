4d7258affacac67429c8a4c0c6c564a96fa083ef
Dekr0 <dekr0.dk@protonmail.com>
2025-07-04 22:47:31 -0600

    - Transport Control test

--- ui/cb_tctrl.go
+++ ui/cb_tctrl.go
@@ -9,9 +9,8 @@ import (
 	"github.com/Dekr0/wwise-teller/waapi"
 )
 
-func createPlayerNoCache(cache *sync.Map, sid uint32, wemData []byte) {
-	ctx, cancel := context.WithCancel(context.Background())
-	callback := func(ctx context.Context) {
+func createPlayerNoCacheTask(cache *sync.Map, sid uint32, wemData []byte) func(context.Context) {
+	return func(ctx context.Context) {
 		tmpWAVPath, err := waapi.WEMToWAVEByte(ctx, wemData)
 		if err != nil {
 			slog.Error(fmt.Sprintf("Failed to transform audio source %d data to WAV", sid), "error", err)
@@ -22,6 +21,11 @@ func createPlayerNoCache(cache *sync.Map, sid uint32, wemData []byte) {
 			slog.Error(fmt.Sprintf("Failed to create audio player for audio source %d (No caching)", sid), "error", err)
 		}
 	}
+}
+
+func createPlayerNoCache(cache *sync.Map, sid uint32, wemData []byte) {
+	ctx, cancel := context.WithCancel(context.Background())
+	callback := createPlayerNoCacheTask(cache, sid, wemData)
 	procMsg := fmt.Sprint("Creating audio player for audio source %d (No caching)", sid)
 	doneMsg := fmt.Sprint("Done creating audio player for audio source %d (No caching)", sid)
 	if err := GlobalCtx.Loop.QTask(ctx, cancel, procMsg, doneMsg, callback); err != nil {
@@ -29,14 +33,18 @@ func createPlayerNoCache(cache *sync.Map, sid uint32, wemData []byte) {
 	}
 }
 
-func createPlayerCache(tmpWAVPath string, sid uint32) {
-	ctx, cancel := context.WithCancel(context.Background())
-	callback := func(ctx context.Context) {
+func createPlayerCacheTask(tmpWAVPath string, sid uint32) func(context.Context) {
+	return func(ctx context.Context) {
 		_, err := GlobalCtx.Manager.Player(tmpWAVPath)
 		if err != nil {
 			slog.Error(fmt.Sprintf("Failed to create audio player for audio source %d (Cached)", sid), "error", err)
 		}
 	}
+}
+
+func createPlayerCache(tmpWAVPath string, sid uint32) {
+	ctx, cancel := context.WithCancel(context.Background())
+	callback := createPlayerCacheTask(tmpWAVPath, sid)
 	procMsg := fmt.Sprint("Creating audio player for audio source %d (Cached)", sid)
 	doneMsg := fmt.Sprint("Done creating audio player for audio source %d (Cached)", sid)
 	if err := GlobalCtx.Loop.QTask(ctx, cancel, procMsg, doneMsg, callback); err != nil {
--- ui/tctrl_test.go
+++ ui/tctrl_test.go
@@ -1,8 +1,51 @@
 package ui
 
-import "testing"
+import (
+	"context"
+	"os"
+	"path/filepath"
+	"sync"
+	"testing"
+
+	"github.com/Dekr0/wwise-teller/parser"
+	"github.com/Dekr0/wwise-teller/utils"
+	"github.com/Dekr0/wwise-teller/waapi"
+)
+
+var testBanksDir = os.Getenv("TESTS")
 
 func TestCreatePlayerNoCache(t *testing.T) {
+	utils.InitTmp()
+	defer utils.CleanTmp()
+	waapi.InitWEMCache()
+	defer waapi.CleanWEMCache()
 
+	bank := "content_audio_wep_ar19_liberator.st_bnk"
+
+	bnk, err := parser.ParseBank(filepath.Join(testBanksDir, bank), context.Background(), false)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var cacheMap sync.Map
+	data := bnk.DATA()
+
+	sid := uint32(279367945)
+	wemData, in := data.AudiosMap[sid]
+	if !in {
+		t.Fatalf("No audio source has ID %d.", sid)
+	}
+
+	task := createPlayerNoCacheTask(&cacheMap, sid, wemData)
+	task(t.Context())
+
+	v, in := cacheMap.Load(sid)
+	if !in {
+		t.Fatalf("Data of audio source %d is not transformed into WAVE and cached", sid)
+	}
+	_, err = GlobalCtx.Manager.Player(v.(string))
+	if err != nil {
+		t.Fatal(err)
+	}
 }
 
