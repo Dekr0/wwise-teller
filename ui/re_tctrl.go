a455120836fa96424ee98278041d02a69d86e000
Dekr0 <dekr0.dk@protonmail.com>
2025-07-05 20:53:19 -0600

    - Semi functional render transport controlling

--- ui/re_tctrl.go
+++ ui/re_tctrl.go
@@ -1,18 +1,29 @@
+// TODO: Rethink how to construct this for concurrency. This is very cooked.
 package ui
 
 import (
+	"fmt"
+	"log/slog"
+
 	"github.com/AllenDang/cimgui-go/imgui"
+	"github.com/Dekr0/wwise-teller/aio"
 	be "github.com/Dekr0/wwise-teller/ui/bank_explorer"
 	"github.com/Dekr0/wwise-teller/wwise"
 )
 
-func RenderTransportControl(t *be.BankTab) {
-	if t == nil || t.Bank == nil || t.Bank.HIRC() == nil || t.WriteLock.Load() {
+func RenderTransportControl(t *be.BankTab, open *bool) {
+	if !*open {
+		return
+	}
+	imgui.BeginV("Transport Control", open, imgui.WindowFlagsNone)
+	defer imgui.End()
+	if !*open {
 		return
 	}
 
-	imgui.Begin("Transport Control")
-	defer imgui.End()
+	if t == nil || t.Bank == nil || t.Bank.HIRC() == nil || t.SounBankLock.Load() {
+		return
+	}
 
 	didx := t.Bank.DIDX()
 	if didx == nil {
@@ -36,19 +47,86 @@ func RenderTransportControl(t *be.BankTab) {
 	}
 
 	sid := sound.BankSourceData.SourceID
-	v, ok := t.WEMToWaveCache.Load(sid)
-	if !ok {
-		wemData, in := data.AudiosMap[sid]
-		if !in { // TODO, FX based audio source
-			return
-		}
-		createPlayerNoCache(&t.WEMToWaveCache, sid, wemData)
+
+	if v, ok := t.ErrorAudioSources.Load(sid); ok && v.(int) >= be.MaxExportRetrys {
+		imgui.Text(fmt.Sprintf("Export is suspended for audio source %d.", sid))
 		return
 	}
-	tmpWAVPath := v.(string)
-	ok = GlobalCtx.Manager.HasPlayer(tmpWAVPath)
+
+	v, ok := t.WEMExportCache.Load(sid)
 	if !ok {
-		createPlayerCache(tmpWAVPath, sid)
+		if t.WEMExportLock.Load() {
+			imgui.ProgressBarV(
+				float32(-1.0 * imgui.Time()),
+				imgui.NewVec2(0, 0),
+				"A background audio source export task is running. Please wait...",
+			)
+			return
+		}
+		wemData, in := data.AudiosMap[sid]
+		// TODO, FX based audio source
+		if !in { 
+			return
+		}
+		createPlayerNoCache(t, sid, wemData)
 		return
+	}
+
+	waveFile := v.(string)
+	mngr := &GlobalCtx.PlayersManager
+	i := mngr.HasPlayer(waveFile)
+	if i == -1 {
+		if mngr.CreateLock.Load() {
+			imgui.ProgressBarV(
+				float32(-1.0 * imgui.Time()),
+				imgui.NewVec2(0, 0),
+				"A background audio player create task is running. Please wait...",
+			)
+			return
+		}
+		createPlayerCache(waveFile, sid)
+		return
+	}
+	if i == -2 {
+		imgui.Text(fmt.Sprintf("Audio player initialization is suspended for audio source %d", sid))
+		return
+	}
+
+	Disabled(!aio.BeepEnable, func() {
+		if imgui.Button("Play") {
+			mngr.SetActivePlayer(waveFile)
+			if err := mngr.PlayActivePlayer(); err != nil {
+				slog.Error( "Failed to play active audio player", "error", err)
+			}
+		}
+		imgui.SameLine()
+		if imgui.Button("Pause") {
+			mngr.SetActivePlayer(waveFile)
+			mngr.PauseActivePlayer()
+		}
+		imgui.SameLine()
+		if imgui.Button("Resume") {
+			mngr.SetActivePlayer(waveFile)
+			mngr.ResumeActivePlayer()
+		}
+		imgui.SameLine()
+		if imgui.Button("Loop") {
+			mngr.SetActivePlayer(waveFile)
+			if err := mngr.LoopActivePlayer(); err != nil {
+				slog.Error("Failed to loop audio cue", "error", err)
+			}
+		}
+	})
+
+	pos, l, format, in := mngr.PlayerInfo(waveFile)
+	if in {
+		fpos := float32(format.SampleRate.D(pos).Seconds())
+		fl := float32(format.SampleRate.D(l).Seconds())
+		imgui.Text(fmt.Sprintf("%f / %f", fpos, fl))
+		imgui.BeginDisabled()
+		imgui.SetNextItemWidth(-1)
+		imgui.SliderFloat("##Timestamp", &fpos, 0.0, fl)
+		imgui.EndDisabled()
+		imgui.Text(fmt.Sprintf("# of Channels: %d", format.NumChannels))
 	}
 }
