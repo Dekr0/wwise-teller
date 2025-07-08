8a359ca2d0c90a2a4567039719e971caab295506
Dekr0 <dekr0.dk@protonmail.com>
2025-07-08 01:30:58 -0600

    - Remove PlayerManager for in the GlobalContext. Each sound bank tab will have its the concept of Session, which is eqivalent of PlayerManager with a slightly better design.

--- ui/context.go
+++ ui/context.go
@@ -1,7 +1,6 @@
 package ui
 
 import (
-	"github.com/Dekr0/wwise-teller/aio"
 	"github.com/Dekr0/wwise-teller/config"
 	"github.com/Dekr0/wwise-teller/ui/async"
 )
@@ -11,7 +10,6 @@ type Context struct {
 	ModalQ         ModalQ
 	Config         config.Config
 	CopyEnable     bool
-	PlayersManager aio.PlayersManager
 }
 
 var GlobalCtx Context = Context{
@@ -19,8 +17,4 @@ var GlobalCtx Context = Context{
 	NewModalQ(),
 	config.Config{},
 	false,
-	aio.PlayersManager{
-		Max: aio.DefaultMaxLRUWEMPlayerManagerSize,
-		Players: make([]*aio.Player, 0, 8),
-	},
 }
