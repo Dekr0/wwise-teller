d3e7df1e2965c0a25d0a041589db8d60de978682
Dekr0 <dekr0.dk@protonmail.com>
2025-07-17 02:27:04 -0600

    Encode function require version parameter instead of relying global bank version value; Support parsing of FxChunk and FxChunkItem for version > 145.

--- wwise/fx_chunk.go
+++ wwise/fx_chunk.go
@@ -9,7 +9,7 @@ import (
 
 type FxChunk struct {
 	// UniqueNumFx uint8
-	BitsFxByPass uint8
+	BitsFxByPass uint8 // > 145 is called bBypassAll
 	FxChunkItems []FxChunkItem
 }
 
@@ -17,34 +17,50 @@ func (f *FxChunk) Clone() FxChunk {
 	return FxChunk{f.BitsFxByPass, slices.Clone(f.FxChunkItems)}
 }
 
-func (f *FxChunk) Encode() []byte {
+func (f *FxChunk) Encode(v int) []byte {
 	if len(f.FxChunkItems) <= 0 {
 		return []byte{0}
 	}
-	size := f.Size()
+	size := f.Size(v)
 	w := wio.NewWriter(uint64(size))
 	w.AppendByte(uint8(len(f.FxChunkItems)))
 	w.AppendByte(f.BitsFxByPass)
 	for _, i := range f.FxChunkItems {
-		w.Append(i)
+		w.Append(i.UniqueFxIndex)
+		w.Append(i.FxId)
+		if v <= 145 {
+			w.Append(i.BitIsShareSet)
+			w.Append(i.BitIsRendered)
+		} else {
+			w.Append(i.BitVector)
+		}
 	}
 	return w.BytesAssert(int(size))
 }
 
-func (f *FxChunk) Size() uint32 {
+func (f *FxChunk) Size(v int) uint32 {
 	if len(f.FxChunkItems) <= 0 {
 		return 1
 	}
-	return uint32(1 + 1 + len(f.FxChunkItems) * SizeOfFxChunk)
+	if v <= 145 {
+		return uint32(1 + 1 + len(f.FxChunkItems) * SizeOfFxChunk_LE145)
+	}
+	return uint32(1 + 1 + len(f.FxChunkItems) * SizeOfFxChunk_G145)
 }
 
-const SizeOfFxChunk = 7
+const SizeOfFxChunk_LE145 = 7
+const SizeOfFxChunk_G145 = 6
 type FxChunkItem struct {
 	UniqueFxIndex uint8 // u8i
 	FxId          uint32 // tid
+	// The following is exclusive
+	// <= 145
 	BitIsShareSet uint8 // U8x
 	BitIsRendered uint8 // U8x unused (effects can't render)
+	// > 145
+	BitVector     uint8
 }
+
 
 type FxChunkMetadata struct {
 	BitIsOverrideParentMetadata uint8
@@ -56,8 +72,8 @@ func (f *FxChunkMetadata) Clone() FxChunkMetadata {
 	return FxChunkMetadata{f.BitIsOverrideParentMetadata, slices.Clone(f.FxMetaDataChunkItems)}
 }
 
-func (f *FxChunkMetadata) Encode() []byte {
-	size := f.Size()
+func (f *FxChunkMetadata) Encode(v int) []byte {
+	size := f.Size(v)
 	w := wio.NewWriter(uint64(size))
 	w.AppendByte(f.BitIsOverrideParentMetadata)
 	w.AppendByte(uint8(len(f.FxMetaDataChunkItems)))
@@ -67,7 +83,7 @@ func (f *FxChunkMetadata) Encode() []byte {
 	return w.BytesAssert(int(size))
 }
 
-func (f *FxChunkMetadata) Size() uint32 {
+func (f *FxChunkMetadata) Size(int) uint32 {
 	return uint32(1 + 1 + len(f.FxMetaDataChunkItems) * SizeOfFxChunkMetadata)
 }
 
