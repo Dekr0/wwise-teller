68ac75b0542d09b251559ee5ab2d96d9d0c3bbbf
Dekr0 <dekr0.dk@protonmail.com>
2025-07-18 05:42:59 -0600

    Passing test for new implementation of sound bank extraction and multi-bank packing for helldivers 2

--- integration/helldivers/lib_stable_test.go
+++ integration/helldivers/lib_stable_test.go
@@ -7,27 +7,49 @@ import (
 	"testing"
 
 	"github.com/Dekr0/wwise-teller/parser"
+	"github.com/Dekr0/wwise-teller/wwise"
 )
 
 func TestExtractSoundBanksStable(t *testing.T) {
 	slog.SetLogLoggerLevel(slog.LevelInfo.Level())
 
 	data := "/mnt/d/Program Files/Steam/steamapps/common/Helldivers 2/data"
-	target := filepath.Join(data, "22749a294788af66")
+	target := filepath.Join(data, "c76c97b3dfb67c5c")
 	if err := ExtractSoundBankStable(target, ".", false); err != nil {
 		t.Fatal(err)
 	}
+}
 
-	bnk, err := parser.ParseBank("./content_audio_vehicle_combat_walker.st_bnk", context.Background(), false)
-	if err != nil {
+func TestGenPatchStable(t *testing.T) {
+	ctx := context.Background()
+	bnksName := []string{
+		"./8342649675791264267.st_bnk",
+	}
+	bnks := []wwise.Bank{}
+	for _, bnkName := range bnksName {
+		bnk, err:= parser.ParseBank(bnkName, ctx, false)
+		if err != nil {
+			t.Fatal(err)
+		}
+		bnks = append(bnks, *bnk)
+	}
+	bnksData := [][]byte{}
+	metas := [][]byte{}
+	for _, bnk := range bnks {
+		bnkData, err := bnk.Encode(ctx, false)
+		if err != nil {
+			t.Fatal(err)
+		}
+		bnksData = append(bnksData, bnkData)
+		meta := bnk.META()
+		if err != nil {
+			t.Fatal(err)
+		}
+		metas = append(metas, meta.B)
+	}
+	if err := GenHelldiversPatchStableMulti(bnksData, metas, "."); err != nil {
 		t.Fatal(err)
 	}
-
-	b, err := bnk.Encode(context.Background(), false)
-	if err != nil {
-		t.Fatal(err)
-	}
-	GenHelldiversPatchStable(b, bnk.META().B, ".")
 }
 
 func TestExtractSoundBanksPatchStable(t *testing.T) {
