6cc437e4b722db14ee8b7cefcd425e04d6f14a66
Dekr0 <dekr0.dk@protonmail.com>
2025-07-19 15:30:11 -0600

    - Error for use of database before database connection pool is open - Ping() function to verify whether if the database connection pool is initialized and the database is available. - A global *sql.DB instance (sqlite3 connection pool) - InitDatabase() function to initialize sqlite3 connection pool, and verify whether if the database is available - CloseDatabase() function to close the gloabl *sql.DB instance (sqlite3 connection pool) - createConn() function to create a new connection / obtain an existing connection from the databsae - createConnWithQuery to create a new connection / obtainan existing connection from the database, and wrap that connection to Query. - createConnWithTxQuery to create a new connection / obtainan existing connection from the database, start a transaction, and wrap that transaction to Query. - move InsertSource into the retry block - move InsertHierarchy into the retry block - AllocateSids function to allocate new source IDs into a preallocated source IDs array - AllocateHids function to allocate new hierarchy IDs into a preallocated hierarchy IDs array - AllocateSounds function to allocate new sounds (source ID and hierarchy ID in pair) into the preallocated source IDs array and hierarchy IDs array

--- db/lib.go
+++ db/lib.go
@@ -20,11 +20,25 @@ import (
 
 const DatabaseEnv = "IDATABASE"
 
-var DatabaseEnvNotSet error = fmt.Errorf("Enviromental variable %s is not set.", DatabaseEnv)
-var DatabaseEnvNotAbs error = fmt.Errorf("Enviromental variable %s is not in absolute path.", DatabaseEnv)
+var DatabaseEnvNotSet   error = fmt.Errorf("Enviromental variable %s is not set.", DatabaseEnv)
+var DatabaseEnvNotAbs   error = fmt.Errorf("Enviromental variable %s is not in absolute path.", DatabaseEnv)
+var DatabaseInitRequire error = errors.New("Wwise sound bank ID database is yet opened and initialized")
 var WriteLock sync.Mutex
 
-func CheckDatabaseEnv() error {
+func Ping() error {
+	if WwiseIdDB == nil {
+		return DatabaseInitRequire
+	}
+	err := WwiseIdDB.Ping()
+	if err != nil {
+		return fmt.Errorf("Failed to verify Connectivity of Wwise sound bank ID database: %w", err) 
+	}
+	return nil
+}
+
+var WwiseIdDB *sql.DB
+// Open the database using the default environemntal variable
+func InitDatabase() (err error) {
 	p := os.Getenv(DatabaseEnv)
 	if p == "" {
 		return DatabaseEnvNotSet
@@ -32,51 +46,57 @@ func CheckDatabaseEnv() error {
 	if !filepath.IsAbs(p) {
 		return DatabaseEnvNotAbs
 	}
-	db, err := CreateDefaultConn()
-	defer db.Close()
+	WwiseIdDB, err = sql.Open("sqlite3", p)
 	if err != nil {
-		return fmt.Errorf("%s is not a valid ID database because of database connection error (%w).", p, err)
+		return fmt.Errorf("Failed to open database %s: %w", p, err)
+	}
+	slog.Info("Opened Wwise sound bank ID database")
+	err = WwiseIdDB.Ping()
+	if err != nil {
+		return fmt.Errorf("Failed to verify Wwise sound bank ID database: %w", err) 
 	}
 	return nil
 }
 
-// Create a database connection using the default environmental variable
-func CreateDefaultConn() (*sql.DB, error) {
-	p := os.Getenv(DatabaseEnv)
-	if p == "" {
-		return nil, DatabaseEnvNotSet
+func CloseDatabase() {
+	if WwiseIdDB != nil {
+		WwiseIdDB.Close()
 	}
-	db, err := sql.Open("sqlite3", p)
-	if err != nil {
-		return nil, fmt.Errorf("Failed to open database %s: %w", p, err)
-	}
-	return db, nil
 }
 
-func CreateDefaultConnWithQuery() (*id.Queries, func(), error) {
-	db, err := CreateDefaultConn()
+// Create a database connection using the default environmental variable
+func createConn(ctx context.Context) (conn *sql.Conn, err error) {
+	if err = Ping(); err != nil {
+		return conn, err
+	}
+	conn, err = WwiseIdDB.Conn(ctx)
+	return conn, fmt.Errorf("Failed to connect to Wwise sound bank ID database: %w", err)
+}
+
+func createConnWithQuery(ctx context.Context) (*id.Queries, func(), error) {
+	conn, err := createConn(ctx)
 	if err != nil {
 		return nil, nil, err
 	}
-	closeDb := func() { 
-		if err := db.Close(); err != nil {
+	closeConn := func() { 
+		if err := conn.Close(); err != nil {
 			slog.Error("Failed to close database connection", "error", err)
 		}
 	}
-	return id.New(db), closeDb, nil
+	return id.New(conn), closeConn, nil
 }
 
-func CreateDefaultConnWithTxQuery(ctx context.Context) (*id.Queries, func(), func() error, func(), error) {
-	db, err := CreateDefaultConn()
+func CreateConnWithTxQuery(ctx context.Context) (*id.Queries, func(), func() error, func(), error) {
+	conn, err := createConn(ctx)
 	if err != nil {
 		return nil, nil, nil, nil, err
 	}
-	tx, err := db.BeginTx(ctx, &sql.TxOptions{})
+	tx, err := conn.BeginTx(ctx, &sql.TxOptions{})
 	if err != nil {
-		return nil, nil, nil, nil, err
+		return nil, nil, nil, nil, fmt.Errorf("Failed begin Wwise sound bank ID database transaction: %w", err)
 	}
-	closeDb := func() { 
-		if err := db.Close(); err != nil {
+	closeConn := func() { 
+		if err := conn.Close(); err != nil {
 			slog.Error("Failed to close database connection", "error", err)
 		}
 	}
@@ -91,7 +111,7 @@ func CreateDefaultConnWithTxQuery(ctx context.Context) (*id.Queries, func(), fun
 			slog.Error("Failed to rollback database transaction. Please manually rollback database by using the backup database", "error", err)
 		}
 	}
-	return id.New(db).WithTx(tx), closeDb, commit, rollback, nil
+	return id.New(conn).WithTx(tx), closeConn, commit, rollback, nil
 }
 
 func TrySid(ctx context.Context, q *id.Queries) (uint32, error) {
@@ -114,15 +134,15 @@ func TrySid(ctx context.Context, q *id.Queries) (uint32, error) {
 			slog.Error(err.Error())
 			return err
 		}
+		if err := q.InsertSource(ctx, int64(sid)); err != nil {
+			return fmt.Errorf("Failed insert the new allocated source ID %d into Wwise sound bank database: %w", sid, err)
+		}
 		return nil
 	}, b); err != nil {
-		return 0, err
+		return 0, fmt.Errorf("Failed to allocate a new source ID after exhausting all retry: %w", err)
 	}
 	if sid == 0 {
 		return 0, errors.New("Source ID uses invalid value of 0.")
-	}
-	if err := q.InsertSource(ctx, int64(sid)); err != nil {
-		return 0, err
 	}
 	return sid, nil
 }
@@ -147,15 +167,106 @@ func TryHid(ctx context.Context, q *id.Queries) (uint32, error) {
 			slog.Error(err.Error())
 			return err
 		}
+		if err := q.InsertHierarchy(ctx, int64(hid)); err != nil {
+			return fmt.Errorf("Failed insert the new allocated hierarchy ID %d into Wwise sound bank database: %w", hid, err)
+		}
 		return nil
 	}, b); err != nil {
-		return 0, err
+		return 0, fmt.Errorf("Failed to allocate a new hierarchy ID after exhausting all retry: %w", err)
 	}
 	if hid == 0 {
 		return 0, errors.New("Source ID uses invalid value of 0.")
 	}
-	if err := q.InsertHierarchy(ctx, int64(hid)); err != nil {
-		return 0, err
-	}
 	return hid, nil
+}
+
+func AllocateSids(ctx context.Context, ids []uint32) (
+	closeConn func(), commit func() error, rollback func(), err error,
+) {
+	if len(ids) <= 0 {
+		return closeConn, commit, rollback, fmt.Errorf("Empty source IDs array is provided")
+	}
+	var q *id.Queries
+	q, closeConn, commit, rollback, err = CreateConnWithTxQuery(ctx)
+	if err != nil {
+		return closeConn, commit, rollback, err
+	}
+	for i := range ids {
+		sid, err := TrySid(ctx, q)
+		if err != nil {
+			rollback()
+			rollback = nil
+			closeConn()
+			closeConn = nil
+			commit = nil
+			return closeConn, commit, rollback, err
+		}
+		ids[i] = sid
+	}
+	return closeConn, commit, rollback, nil
+}
+
+func AllocateHids(ctx context.Context, ids []uint32) (
+	closeConn func(), commit func() error, rollback func(), err error,
+) {
+	if len(ids) <= 0 {
+		return closeConn, commit, rollback, fmt.Errorf("Empty hierarchy IDs array is provided")
+	}
+	var q *id.Queries
+	q, closeConn, commit, rollback, err = CreateConnWithTxQuery(ctx)
+	if err != nil {
+		return closeConn, commit, rollback, err
+	}
+	for i := range ids {
+		hid, err := TryHid(ctx, q)
+		if err != nil {
+			rollback()
+			rollback = nil
+			closeConn()
+			closeConn = nil
+			commit = nil
+			return closeConn, commit, rollback, err
+		}
+		ids[i] = hid
+	}
+	return closeConn, commit, rollback, nil
+}
+
+func AllocateSounds(ctx context.Context, sids []uint32, hids []uint32) (
+	closeConn func(), commit func() error, rollback func(), err error,
+) {
+	if len(sids) != len(hids) {
+		return closeConn, commit, rollback, fmt.Errorf("Length of source IDs array and lenght of hierarchy IDs mismatch")
+	}
+	if len(sids) == 0 {
+		return closeConn, commit, rollback, fmt.Errorf("Empty hierarchy IDs array is provided")
+	}
+	var q *id.Queries
+	q, closeConn, commit, rollback, err = CreateConnWithTxQuery(ctx)
+	if err != nil {
+		return closeConn, commit, rollback, err
+	}
+	for i := range sids {
+		sid, err := TrySid(ctx, q)
+		if err != nil {
+			rollback()
+			rollback = nil
+			closeConn()
+			closeConn = nil
+			commit = nil
+			return closeConn, commit, rollback, err
+		}
+		sids[i] = sid
+		hid, err := TryHid(ctx, q)
+		if err != nil {
+			rollback()
+			rollback = nil
+			closeConn()
+			closeConn = nil
+			commit = nil
+			return closeConn, commit, rollback, err
+		}
+		hids[i] = hid
+	}
+	return closeConn, commit, rollback, nil
 }
