257e285798d8b1130f1fef4110cd2b94b1989025
Dekr0 <dekr0.dk@protonmail.com>
2025-07-25 04:06:14 -0600

    work in progress of rendering function layer data

---
+++ ui/re_layer.go
@@ -0,0 +1,23 @@
+package ui
+
+import (
+	"fmt"
+
+	"github.com/AllenDang/cimgui-go/imgui"
+	"github.com/Dekr0/wwise-teller/wwise"
+)
+
+func renderLayer(layerCntr *wwise.LayerCntr) {
+	fmt.Println(len(layerCntr.Layers))
+	if imgui.TreeNodeExStr("Layer / Blend Tracks") {
+		if imgui.BeginTabBarV("LayerTabBar", DefaultTabFlags) {
+			for _, layer := range layerCntr.Layers  {
+				if imgui.BeginTabItem(fmt.Sprintf("Layer / Blend Track %d", layer.Id)) {
+					imgui.EndTabItem()
+				}
+			}
+			imgui.EndTabBar()
+		}
+		imgui.TreePop()
+	}
+}
