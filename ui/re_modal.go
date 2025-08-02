abd0c96ff4ea1bae9db38b0561f87b19d9cd9326
Dekr0 <dekr0.dk@protonmail.com>
2025-08-02 05:09:14 -0600

    - Untie rendering function from ModalQ's method - move logical callback functions to all the associate render files - Application wise bank manager - Application wise background context

--- ui/re_modal.go
+++ ui/re_modal.go
@@ -1,55 +1,43 @@
 package ui
 
 import (
-	"fmt"
 	"log/slog"
 
 	"github.com/AllenDang/cimgui-go/imgui"
-	"github.com/Dekr0/wwise-teller/integration/helldivers"
-	be "github.com/Dekr0/wwise-teller/ui/bank_explorer"
+	"github.com/Dekr0/wwise-teller/ui/modal"
 )
 
-func (m *ModalQ) renderModal() {
+func renderModal(m *modal.ModalQ) {
 	if len(m.Modals) <= 0 {
 		return
 	}
 	top := m.Modals[len(m.Modals)-1]
-	if *top.done {
+	if *top.Done {
 		imgui.CloseCurrentPopup()
 		m.Modals = m.Modals[:len(m.Modals)-1]
-		if top.onClose != nil {
-			top.onClose()
+		if top.OnClose != nil {
+			top.OnClose()
 		}
 		return
 	}
-	if !imgui.IsPopupOpenStr(top.name) {
-		imgui.OpenPopupStr(top.name)
+	if !imgui.IsPopupOpenStr(top.Name) {
+		imgui.OpenPopupStr(top.Name)
 		imgui.SetNextWindowSize(imgui.NewVec2(640, 640))
 	}
 	center := imgui.MainViewport().Center()
 	imgui.SetNextWindowPosV(center, imgui.CondAppearing, imgui.NewVec2(0.5, 0.5))
-	if imgui.BeginPopupModalV(top.name, nil, top.flag) {
+	if imgui.BeginPopupModalV(top.Name, nil, top.Flag) {
 		if imgui.Shortcut(imgui.KeyChord(imgui.ModCtrl) | imgui.KeyChord(imgui.KeyQ)) {
-			*top.done = true
+			*top.Done = true
 		}
-		top.loop()
+		top.Loop()
 		imgui.EndPopup()
 	}
 }
 
-func pushConfigModalFunc() {
-	renderF, done := configModalFunc()
-	GlobalCtx.ModalQ.QModal(
-		done,
-		imgui.WindowFlagsAlwaysAutoResize,
-		"Config",
-		renderF, nil,
-	)
-}
-
 func pushSetHomeModal() {
 	onSave := func(path string) {
-		if err := GlobalCtx.Config.SetHome(path); err != nil {
+		if err := GCtx.Config.SetHome(path); err != nil {
 			slog.Error(
 				"Failed to set initial directory for file " +
 				"explorer",
@@ -57,7 +45,7 @@ func pushSetHomeModal() {
 			)
 		}
 	}
-	renderF, done, err := saveFileDialogFunc(onSave, GlobalCtx.Config.Home)
+	renderF, done, err := saveFileDialogFunc(onSave, GCtx.Config.Home)
 	if err != nil {
 		slog.Error(
 			"Failed to create save file dialog for setting initial" + 
@@ -65,128 +53,22 @@ func pushSetHomeModal() {
 			"error", err,
 		)
 	} else {
-		GlobalCtx.ModalQ.QModal(
-			done,
-			0,
-			"Set starting directory for file explorer",
-			renderF, nil,
-		)
+		Modal(done, 0, "Set starting directory for file explorer", renderF, nil)
 	}
 }
 
-func pushSaveSoundBankModal(
-	bnkMngr *be.BankManager,
-	saveTab *be.BankTab,
-	saveName string,
-	excludeMeta bool,
-) {
-	onSave := saveSoundBankFunc(bnkMngr, saveTab, saveName, excludeMeta)
-	renderF, done, err := saveFileDialogFunc(onSave, GlobalCtx.Config.Home)
-	if err != nil {
-		slog.Error(
-			fmt.Sprintf("Failed create save file dialog for saving sound bank %s",
-				saveName,
-			),
-			"error", err,
-		)
-	} else {
-		GlobalCtx.ModalQ.QModal(
-			done,
-			0,
-			fmt.Sprintf("Save sound bank %s to ...", saveName),
-			renderF, nil,
-		)
-	}
-}
-
-func pushHD2PatchModal(
-	bnkMngr *be.BankManager,
-	saveTab *be.BankTab,
-	saveName string,
-) {
-	onSave := HD2PatchFunc(bnkMngr, saveTab, saveName)
-	if renderF, done, err := saveFileDialogFunc(onSave, GlobalCtx.Config.Home);
-	   err != nil {
-		slog.Error(
-			fmt.Sprintf("Failed create save file dialog for saving sound " +
-				"bank %s to HD2 patch", saveName,
-			),
-			"error", err,
-		)
-	} else {
-		GlobalCtx.ModalQ.QModal(
-			done,
-			0,
-			fmt.Sprintf("Save sound bank %s to HD2 patch ...", saveName),
-			renderF, nil,
-		)
-	}
-}
-
-func pushSelectGameArchiveModal() {
-	onOpen := selectHD2ArchiveFunc()
-	initialDir, err := helldivers.GetHelldivers2Data()
-	if err != nil {
-		initialDir = GlobalCtx.Config.Home
-	}
-	renderF, done, err := openFileDialogFunc(
-		onOpen, false, initialDir, []string{},
-	)
-	if err != nil {
-		slog.Error(
-			"Failed to create open file dialog for opening " +
-			"Helldivers 2 game archives",
-			"error", err,
-		)
-	} else {
-		GlobalCtx.ModalQ.QModal(
-			done, 
-			0,
-			"Select Helldivers 2 game archives",
-			renderF, nil,
-		)
-	}
-}
-
-func pushExtractSoundBanksModal(paths []string) {
-	onSave := extractHD2SoundBanksFunc(paths)
-	renderF, done, err := saveFileDialogFunc(onSave, GlobalCtx.Config.Home)
-	if err != nil {
-		slog.Error(
-			"Failed create save file dialog for saving extracted sound banks",
-			"error", err,
-		)
-		return
-	}
-	GlobalCtx.ModalQ.QModal(
-		done,
-		0,
-		"Save extracted sound banks to ...",
-		renderF, nil,
-	)
-}
-
-func pushCommandPaletteModal(cmdPaletteMngr *CmdPaletteMngr) {
-	renderF, done := commandPaletteModal(cmdPaletteMngr)
-	GlobalCtx.ModalQ.QModal(done, 0, "Command Palette", renderF, nil)
-}
-
-func pushSimpleTextModal(title string, c func(string)) {
-	renderF, done := simpleTextModal(c)
-	GlobalCtx.ModalQ.QModal(done, imgui.WindowFlagsAlwaysAutoResize, title, renderF, nil)
-}
-
-func simpleTextModal(c func(string)) (func(), *bool) {
+func PushSimpleTextModal(title string, label string, confirm string, c func(string)) {
 	done := false
 	text := ""
-	return func() {
+	f := func() {
 		imgui.SetNextItemShortcut(imgui.KeyChord(imgui.ModCtrl) | imgui.KeyChord(imgui.KeyF))
-		imgui.InputTextWithHint("Directory Name", "", &text, 0, nil)
+		imgui.InputTextWithHint(label, "", &text, 0, nil)
 		imgui.SameLine()
 		imgui.SetNextItemShortcut(imgui.KeyChord(imgui.ModCtrl) | imgui.KeyChord(imgui.KeyS))
-		if imgui.Button("Create") {
+		if imgui.Button(confirm) {
 			done = true
 			c(text)
 		}
-	}, &done
+	}
+	Modal(&done, imgui.WindowFlagsAlwaysAutoResize, title, f, nil)
 }
