b83e3c1cce0ef979b6db5bab62dd326be0fbfd37
Dekr0 <dekr0.dk@protonmail.com>
2025-08-02 04:56:23 -0600

    - Application wise bank manager - Application wise background context

--- ui/context/context.go
+++ ui/context/context.go
@@ -1,17 +1,23 @@
 package Context
 
 import (
+	"context"
+	"sync/atomic"
+
 	"github.com/Dekr0/wwise-teller/config"
 	"github.com/Dekr0/wwise-teller/ui/async"
+	"github.com/Dekr0/wwise-teller/ui/bank_explorer"
 	dockmanager "github.com/Dekr0/wwise-teller/ui/dock_manager"
 	"github.com/Dekr0/wwise-teller/ui/modal"
 	"github.com/Dekr0/wwise-teller/ui/processor"
 )
 
 type Context struct {
+	Ctx        context.Context
 	Loop       async.EventLoop
 	ModalQ     modal.ModalQ
 	Config     config.Config
+	BankMngr   bank_explorer.BankManager
 	DockMngr   dockmanager.DockManager
 	Editor     processor.ProcessorEditor
 	CopyEnable bool
@@ -20,10 +26,13 @@ type Context struct {
 var GCtx Context = Context{}
 
 func Init() {
+	GCtx.Ctx = context.Background()
 	GCtx.Loop = async.NewEventLoop()
 	GCtx.ModalQ = modal.NewModalQ()
 	GCtx.Config = config.Config{}
 	GCtx.Editor = processor.New()
 	GCtx.CopyEnable = false
+	GCtx.BankMngr = bank_explorer.BankManager{WriteLock: atomic.Bool{}}
+	GCtx.BankMngr.WriteLock.Store(false)
 	dockmanager.NewDockManagerP(&GCtx.DockMngr)
 }
