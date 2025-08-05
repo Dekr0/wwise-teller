90598da5e745fc4e276e637c38ad2ac3b4d3fcbc
Dekr0 <dekr0.dk@protonmail.com>
2025-08-05 17:15:29 -0600

    - Rename FilesToSoundsHeader to WavSoundMapHeader - Work in progress struct to capture Wave file - Sound object mapping

--- automation/replace.go
+++ automation/replace.go
@@ -4,9 +4,13 @@ import (
 	"context"
 	"encoding/csv"
 	"fmt"
+	"io"
 	"log/slog"
 	"os"
 	"path/filepath"
+	"slices"
+	"strconv"
+	"strings"
 
 	"github.com/Dekr0/wwise-teller/waapi"
 	"github.com/Dekr0/wwise-teller/wwise"
@@ -38,7 +42,7 @@ func ReplaceAudioSources(
 
 	reader := csv.NewReader(f)
 	iniDir := filepath.Base(mappingFile)
-	header := FilesToSoundsHeader{
+	header := WavSoundMapHeader{
 		Workspace: iniDir,
 		Type: 0,
 	}
@@ -129,5 +133,132 @@ func ReplaceAudioSources(
 	if err := bnk.CheckDIDXDATA(); err != nil {
 		return err
 	}
+	return nil
+}
+
+type WavSoundMap struct {
+	Header     WavSoundMapHeader
+	Wavs     []string
+	SoundIDs [][]uint32
+}
+
+func (p *WavSoundMap) Encode(out string) error {
+	var b strings.Builder
+	b.WriteString(p.Header.Conversion + "\n")
+	b.WriteString(strconv.FormatUint(uint64(p.Header.Format), 10) + "\n")
+	b.WriteString(strconv.FormatUint(uint64(p.Header.Type), 10) + "\n")
+	b.WriteString(p.Header.Workspace + "\n")
+
+	for i := range p.Wavs {
+		b.WriteString(fmt.Sprintf("%s,%d,", p.Wavs[i], len(p.SoundIDs[i])))
+		for j := range p.SoundIDs[i] {
+			b.WriteString(strconv.FormatUint(uint64(p.SoundIDs[i][j]), 10) + ",")
+			if j < len(p.SoundIDs) - 1 {
+				b.WriteByte(',')
+			}
+		}
+		if i < len(p.Wavs) - 1 {
+			b.WriteByte('\n')
+		}
+	}
+
+	return os.WriteFile(out, []byte(b.String()), 0666) 
+}
+
+func DecodeReplaceAudioSourcesScript(p *WavSoundMap, fspec string) error {
+	f, err := os.Open(fspec)
+	if err != nil {
+		return err
+	}
+
+	reader := csv.NewReader(f)
+	iniDir := filepath.Base(fspec)
+	p.Header.Workspace = iniDir
+	p.Header.Type = 0
+	
+	rowNum := uint16(0)
+	if err = ParseFilesToSoundsHeader(&p.Header, reader, &rowNum); err != nil {
+		return err
+	}
+
+	var row []string
+	var input, ext string
+	for {
+		row, err = reader.Read()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+		}
+
+		if len(row) < 2 {
+			slog.Error(fmt.Sprintf("Expecting two columns, file name and # of targeting sound IDs, at row %d", rowNum))
+			rowNum += 1
+		}
+
+		input = row[0]
+		ext = filepath.Ext(input)
+		if ext == "" {
+			ext = ".wav"
+			input += ext
+		}
+		if ext != ".wav" {
+			slog.Error("Wave file is the only supported file format.")
+			rowNum += 1
+			continue
+		}
+
+		if !filepath.IsAbs(input) {
+			input = filepath.Join(p.Header.Workspace, input)
+		}
+
+		_, err = os.Lstat(input)
+		if err != nil {
+			slog.Error(fmt.Sprintf("Failed to obtain info of wave file %s", input))
+			rowNum += 1
+		}
+
+		count, err := strconv.ParseUint(row[1], 10, 8)
+		if err != nil {
+			slog.Error("Failed to parse # of targeting sound IDs", "error", err)
+			continue
+		}
+		if count > uint64(len(row) - 2) {
+			slog.Error(fmt.Sprintf("Expecting %d of targeting sound IDs but receiving %d", count, len(row)- 2))
+			continue
+		}
+
+		idx := slices.Index(p.Wavs, input)
+		columnNum := 3
+		if idx == -1 {
+			soundIDs := make([]uint32, 0, count)
+			for _, i := range row[2:] {
+				id, err := strconv.ParseUint(i, 10, 32)
+				if err != nil {
+					slog.Error(fmt.Sprintf("Failed to parse sound ID at column %d", columnNum), "error", err)
+					columnNum += 1
+					continue
+				}
+				soundIDs = append(soundIDs, uint32(id))
+				columnNum += 1
+			}
+			p.SoundIDs = append(p.SoundIDs, soundIDs)
+		} else {
+			slog.Warn(fmt.Sprintf("Duplicated input file %s at row %d, column 1", input, rowNum))
+			for _, i := range row[2:] {
+				id, err := strconv.ParseUint(i, 10, 32)
+				if err != nil {
+					slog.Error(fmt.Sprintf("Failed to parse sound ID at column %d", columnNum), "error", err)
+					columnNum += 1
+					continue
+				}
+				p.SoundIDs[idx] = append(p.SoundIDs[idx], uint32(id))
+				columnNum += 1
+			}
+		}
+
+		rowNum += 1
+	}
+
 	return nil
 }
