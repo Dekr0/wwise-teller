47fb9bfed6b3d4f4f06372fd0ece99efd2b2a542
Dekr0 <dekr0.dk@protonmail.com>
2025-08-07 13:25:30 -0600

    - wip attenuation viewer

--- ui/re_attenuation.go
+++ ui/re_attenuation.go
@@ -1,11 +1,13 @@
 package ui
 
 import (
+	"fmt"
 	"strconv"
 
 	"github.com/AllenDang/cimgui-go/imgui"
 	"github.com/AllenDang/cimgui-go/utils"
 	be "github.com/Dekr0/wwise-teller/ui/bank_explorer"
+	dockmanager "github.com/Dekr0/wwise-teller/ui/dock_manager"
 	"github.com/Dekr0/wwise-teller/wwise"
 	"golang.design/x/clipboard"
 )
@@ -81,4 +83,48 @@ func renderAttenuationTableCtx(t *be.BankTab, o wwise.HircObj, id uint32) {
 			clipboard.Write(clipboard.FmtText, []byte(strconv.FormatUint(uint64(id), 10)))
 		}
 	})
+}
+
+func renderAttenuationViewer(open *bool) {
+	if !*open {
+		return
+	}
+
+	imgui.BeginV(dockmanager.DockWindowNames[dockmanager.AttenuationsTag], open, imgui.WindowFlagsNone)
+	defer imgui.End()
+	if !*open{
+		return
+	}
+
+	activeBank, valid := BnkMngr.ActiveBankV()
+	if !valid || activeBank.SounBankLock.Load() {
+		return
+	}
+	if activeBank.AttenuationViewer.ActiveAttenuation != nil {
+		renderAttenuation(activeBank.AttenuationViewer.ActiveAttenuation)
+	}
+}
+
+func renderAttenuation(a *wwise.Attenuation) {
+	imgui.Text(fmt.Sprintf("Attenuation ID %d", a.Id))
+
+	heightSpreadEnabled := a.HeightSpreadEnabled()
+	if imgui.Checkbox("Enable Height Spread", &heightSpreadEnabled) {
+		a.SetHeightSpreadEnabled(heightSpreadEnabled)
+	}
+
+	if imgui.TreeNodeExStr("Cone Attenuation") {
+		imgui.BeginDisabled()
+		coneEnabled := a.ConeEnabled()
+		if imgui.Checkbox("Cone Used", &coneEnabled) {}
+		imgui.PushItemWidth(160.0)
+		imgui.SliderFloat("Cone Max Attenuation", &a.OutsideVolume, -360.0, 360.0)
+		imgui.SliderFloat("Cone Inner Angle Degree", &a.InsideDegrees, -360.0, 360.0)
+		imgui.SliderFloat("Cone Outer Angle Degree", &a.OutsideDegrees, -360.0, 360.0)
+		imgui.SliderFloat("Cone LPF", &a.LoPass, -8192.0, 8192.0)
+		imgui.SliderFloat("Cone HPF", &a.HiPass, -8192.0, 8192.0)
+		imgui.PopItemWidth()
+		imgui.EndDisabled()
+		imgui.TreePop()
+	}
 }
