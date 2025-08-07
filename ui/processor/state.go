447b60dd9c9ecc5d9225ac7d1a92ba126847e029
Dekr0 <dekr0.dk@protonmail.com>
2025-08-07 13:25:48 -0600

    - rethink how load processor should work

--- ui/processor/state.go
+++ ui/processor/state.go
@@ -1,7 +1,6 @@
 package processor
 
 import (
-	"log/slog"
 	"path/filepath"
 
 	"github.com/Dekr0/wwise-teller/automation"
@@ -34,33 +33,5 @@ func (p *ProcessorEditor) NewProcessor(name string) {
 }
 
 func (p *ProcessorEditor) LoadProcessor(fspec string) error {
-	spec, err := automation.ParseProcessor(fspec)
-	if err != nil {
-		return err
-	}
-
-	for _, pipeline := range spec.Pipelines {
-		for _, script := range pipeline.Scripts {
-			scriptPath := filepath.Join(pipeline.BanksWorkspace, script.Script)
-			if _, in := p.ProcessScripts[scriptPath]; in {
-				continue
-			}
-			var err error
-			switch script.Type {
-			case automation.TypeRewireWithNewSources, automation.TypeReplaceAudioSources:
-				s := automation.WavSoundMap{}
-				if err = automation.DecodeReplaceAudioSourcesScript(&s, scriptPath); err != nil {
-					slog.Error("Failed to decode replace audio ")
-
-				}
-				p.ProcessScripts[scriptPath] = &s
-			case automation.TypeBasePropModifiers:
-			case automation.TypeImportAsRanSeqCntr:
-			case automation.TypeRanSeqModifiers:
-			case automation.TypeNewSoundToRanSeqCntr:
-			case automation.TypeBulkProcessBaseProp:
-			}
-		}
-	}
 	return nil
 }
