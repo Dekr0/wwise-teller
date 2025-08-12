f14cdf56d30c35e5ee7b7bfed736570107d2e276
Dekr0 <dekr0.dk@protonmail.com>
2025-08-12 13:36:18 -0600

    render RTPC point (not curve)

--- ui/re_oe_rtpc.go
+++ ui/re_oe_rtpc.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 
 	"github.com/AllenDang/cimgui-go/imgui"
+	"github.com/AllenDang/cimgui-go/implot"
+	"github.com/AllenDang/cimgui-go/utils"
 	"github.com/Dekr0/wwise-teller/wwise"
 )
 
@@ -72,7 +74,32 @@ func renderRTPC(hid uint32, r *wwise.RTPC, title string) {
 				}
 				imgui.EndDisabled()
 
-				renderRTPCGraph(ri.RTPCID, ri.RTPCCurveID, wwise.RTPCParameterType(ri.ParamID.Value), ri.RTPCGraphPoints)
+				renderRTPCGraph(
+					ri.RTPCID,
+					ri.RTPCCurveID,
+					wwise.RTPCParameterType(ri.ParamID.Value),
+					ri.RTPCGraphPointsX,
+					ri.RTPCGraphPointsY,
+					ri.RTPCGraphPointsInterp,
+				)
+
+				const plotFlags = implot.FlagsNoLegend | 
+					              implot.FlagsNoTitle  | 
+							      implot.FlagsNoFrame  |
+					              implot.FlagsCanvasOnly
+				const axisFlags = implot.AxisFlagsNoLabel | 
+					              implot.AxisFlagsAutoFit
+				
+				if implot.BeginPlotV(fmt.Sprintf("%dRTPCGraph%dPlot", hid, i), imgui.Vec2{X: -1, Y: 128}, plotFlags) {
+					implot.SetupAxesV("", "", axisFlags, axisFlags)
+					implot.PlotScatterFloatPtrFloatPtr(
+						fmt.Sprintf("%dRTPCGraph%dScatter", hid, i),
+						utils.SliceToPtr(ri.RTPCGraphPointsX),
+						utils.SliceToPtr(ri.RTPCGraphPointsY),
+						int32(len(ri.RTPCGraphPointsX)),
+					)
+					implot.EndPlot()
+				}
 
 				imgui.TreePop()
 			}
@@ -92,7 +119,9 @@ func renderRTPCGraph(
 	rtpcID uint32,
 	curveID uint32,
 	paramID wwise.RTPCParameterType,
-	points []wwise.RTPCGraphPoint,
+	pointsX []float32,
+	pointsY []float32,
+	pointsInterp []uint32,
 ) {
 	const flags = DefaultTableFlags | imgui.TableFlagsScrollY
 	size := imgui.NewVec2(0, 160)
@@ -109,14 +138,16 @@ func renderRTPCGraph(
 		imgui.TableHeadersRow()
 
 		var stackID string = fmt.Sprintf("%d%d", rtpcID, curveID)
-		for i := range points {
-			p := &points[i]
+		for i := range pointsX {
+			x := &pointsX[i]
+			y := &pointsY[i]
+			interp := &pointsInterp[i]
 
 			imgui.TableNextRow()
 
 			imgui.TableSetColumnIndex(0)
 			imgui.SetNextItemWidth(40)
-			imgui.BeginDisabledV(i == 0 || i >= len(points) - 1)
+			imgui.BeginDisabledV(i == 0 || i >= len(pointsX) - 1)
 			imgui.PushIDStr(fmt.Sprintf("%sRmPoint%d", stackID, i))
 			if imgui.Button("X") {}
 			imgui.PopID()
@@ -124,7 +155,7 @@ func renderRTPCGraph(
 
 			imgui.TableSetColumnIndex(1)
 			imgui.SetNextItemWidth(40)
-			imgui.BeginDisabledV(i >= len(points) - 1)
+			imgui.BeginDisabledV(i >= len(pointsX) - 1)
 			imgui.PushIDStr(fmt.Sprintf("%sAppendPoint%d", stackID, i))
 			if imgui.Button("+") {}
 			imgui.PopID()
@@ -132,44 +163,43 @@ func renderRTPCGraph(
 
 			imgui.TableSetColumnIndex(2)
 			imgui.SetNextItemWidth(128)
-			imgui.BeginDisabledV(i == 0 || i >= len(points) - 1)
-			if i == 0 || i >= len(points) - 1 {
+			if i == 0 || i >= len(pointsX) - 1 {
+				imgui.BeginDisabledV(i == 0 || i >= len(pointsX) - 1)
 				imgui.SliderFloat(
 					fmt.Sprintf("##%sFromSlider%d", stackID, i),
-					&p.From,
+					x,
 					-8192.0,
 					8192,
 				)
+				imgui.EndDisabled()
 			} else {
 				imgui.SliderFloat(
 					fmt.Sprintf("##%sFromSlider%d", stackID, i),
-					&p.From,
-					points[i - 1].From + 1e-4,
-					points[i + 1].From - 1e-4,
+					x,
+					pointsX[i - 1] + 1e-4,
+					pointsX[i + 1] - 1e-4,
 				)
 			}
-			imgui.EndDisabled()
 
 			imgui.TableSetColumnIndex(3)
 			imgui.SetNextItemWidth(128)
 			imgui.BeginDisabledV(!ModifiyEverything)
 			imgui.SliderFloat(
 				fmt.Sprintf("##%sToSlider%d", stackID, i),
-				&p.To, -8192.0, 8192.0,
+				y, -8192.0, 8192.0,
 			)
 			imgui.EndDisabled()
 
 			imgui.TableSetColumnIndex(4)
 			imgui.SetNextItemWidth(-1)
-			curve := int32(p.Interp)
+			curve := int32(*interp)
 			if imgui.ComboStrarr(
 				fmt.Sprintf("##%sRTPCCurve%d", stackID, i),
 				&curve, wwise.InterpCurveTypeName, int32(wwise.InterpCurveTypeCount),
 			) {
-				p.Interp = uint32(curve)
+				*interp = uint32(curve)
 			}
 		}
-
 		imgui.EndTable()
 	}
 }
