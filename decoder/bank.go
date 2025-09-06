a02b61c66dd9f63378233c0ee789ce84ae29397e
Dekr0 <dekr0.dk@protonmail.com>
2025-09-06 05:11:45 -0600

    - Include a version of which read the entire sound bank into memory if sound bank is sufficiently small where streaming will introduce more overhead. - Scaffolding for main flow of chunk decoding

--- decoder/bank.go
+++ decoder/bank.go
@@ -2,13 +2,16 @@ package decoder
 
 import (
 	"bufio"
+	"bytes"
 	"context"
+	"io"
 	"os"
 
+	uio "github.com/Dekr0/unwise/io"
 	"github.com/Dekr0/unwise/wwise"
 )
 
-func Decode(
+func DecodeStreaming(
 	ctx  context.Context, 
 	p    string, 
 	o    order,
@@ -46,6 +49,91 @@ func Decode(
 		return nil, err
 	}
 	wwise.BankAddBKHD(b, bkhd)
-	
+
+	for {
+		_, err = reader.Read(chunkNameBytes)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return nil, err
+		}
+		chunkName = wwise.ChunkName(chunkNameBytes)
+		
+		switch chunkName {
+		default:
+			size, err := uio.U32(reader, o)
+			if err != nil {
+				return nil, err
+			}
+			_, err = reader.Discard(int(size))
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
+	return b, nil
+}
+
+func DecodeMem(
+	ctx  context.Context, 
+	p    string, 
+	o    order,
+	opt *DecoderOption,
+) (b *wwise.Bank, err error) {
+	if opt == nil {
+		opt = &DecoderOption{DecoderBufferSize: DecodeBufferSize}
+		opt.IncludeDATA()
+		opt.IncludeMETA()
+	}
+
+	b = wwise.NewBank()
+
+	mem, err := os.ReadFile(p)
+	if err != nil {
+		return nil, err
+	}
+
+	reader := bytes.NewReader(mem)
+
+	var chunkNameBytes []byte = make([]byte, 4, 4)
+	_, err = reader.Read(chunkNameBytes)
+	if err != nil {
+		return nil, err
+	}
+	chunkName := wwise.ChunkName(chunkNameBytes)
+
+	if chunkName != wwise.ChunkNameBKHD {
+		return nil, WrongBKHDPosition(p)
+	}
+
+	bkhd, err := DecodeBKHD(p, reader, o)
+	if err != nil {
+		return nil, err
+	}
+	wwise.BankAddBKHD(b, bkhd)
+
+	for {
+		_, err = reader.Read(chunkNameBytes)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return nil, err
+		}
+		chunkName = wwise.ChunkName(chunkNameBytes)
+		
+		switch chunkName {
+		default:
+			size, err := uio.U32(reader, o)
+			if err != nil {
+				return nil, err
+			}
+			_, err = reader.Seek(1, int(size))
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
 	return b, nil
 }
