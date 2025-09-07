20d4f9486ae8d1e5ec5f51ae1794cc6050a0002a
Dekr0 <dekr0.dk@protonmail.com>
2025-09-07 14:05:09 -0600

    - remove in memory decoding. Instead, use very large buffer size for large files.

--- decoder/bank.go
+++ decoder/bank.go
@@ -2,17 +2,15 @@ package decoder
 
 import (
 	"bufio"
-	"bytes"
 	"context"
 	"io"
-	"log"
 	"os"
 
 	uio "github.com/Dekr0/unwise/io"
 	"github.com/Dekr0/unwise/wwise"
 )
 
-func DecodeStreaming(
+func Decode(
 	ctx  context.Context, 
 	p    string, 
 	o    order,
@@ -73,76 +71,5 @@ func DecodeStreaming(
 			}
 		}
 	}
-	return b, nil
-}
-
-func DecodeMem(
-	ctx  context.Context, 
-	p    string, 
-	o    order,
-	opt *DecoderOption,
-) (b *wwise.Bank, err error) {
-	if opt == nil {
-		opt = &DecoderOption{DecoderBufferSize: DecodeBufferSize}
-		opt.IncludeDATA()
-		opt.IncludeMETA()
-	}
-
-	b = wwise.NewBank()
-
-	mem, err := os.ReadFile(p)
-	if err != nil {
-		return nil, err
-	}
-
-	reader := bytes.NewReader(mem) // len(mem) is equal to bytes.Reader.Size()
-
-	var chunkNameBytes []byte = make([]byte, 4, 4)
-	_, err = reader.Read(chunkNameBytes)
-	if err != nil {
-		return nil, err
-	}
-	chunkName := wwise.ChunkName(chunkNameBytes)
-
-	if chunkName != wwise.ChunkNameBKHD {
-		return nil, WrongBKHDPosition(p)
-	}
-
-	bkhd, err := DecodeBKHD(p, reader, o)
-	if err != nil {
-		return nil, err
-	}
-	wwise.BankAddBKHD(b, bkhd)
-
-	pos := u8(1)
-	for {
-		_, err = reader.Read(chunkNameBytes)
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			return nil, err
-		}
-		chunkName = wwise.ChunkName(chunkNameBytes)
-		
-		switch chunkName {
-		default:
-			size, err := uio.U32(reader, o)
-			if err != nil {
-				return nil, err
-			}
-
-			// Use slicing to avoid copying
-			// Total size - bytes.Reader.Len() = current postion
-			// Slice the backing buffer from current position to 
-			// current position + chunk size
-			if _, err = reader.Seek(int64(size), io.SeekCurrent); err != nil {
-				return nil, err
-			}
-		}
-
-		pos += 1
-	}
-
 	return b, nil
 }
