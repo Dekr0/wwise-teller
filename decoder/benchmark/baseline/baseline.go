24e0acf80474f772d180fe31f3ab7c4a30c95849
Dekr0 <dekr0.dk@protonmail.com>
2025-09-08 15:01:53 -0600

    - Functions for raw write benchmarking

--- decoder/benchmark/baseline/baseline.go
+++ decoder/benchmark/baseline/baseline.go
@@ -2,7 +2,9 @@ package baseline
 
 import (
 	"bufio"
+	"fmt"
 	"io"
+	"log"
 	"os"
 )
 
@@ -54,4 +56,61 @@ func UnbufferRead(recvSize uint32, p string) {
 			}
 		}
 	}
+}
+
+func WriteOnce(data []byte) (f *os.File, err error) {
+	f, err = os.CreateTemp("", "tmp")
+	if err != nil {
+		return nil, err
+	}
+	_, err = f.Write(data)
+	return f, err
+}
+
+func UnBufferWrite(data []byte, writeSize uint32) (f *os.File, err error) {
+	f, err = os.Create("tmp")
+	if err != nil {
+		return nil, err
+	}
+
+	for len(data) > 0 && err != nil {
+		if int(writeSize) > len(data) {
+			_, err = f.Write(data)
+		} else {
+			_, err = f.Write(data[:writeSize])
+			data = data[writeSize:]
+		}
+	}
+
+	return f, err
+}
+
+func BufferWrite(data []byte, bufSize uint32) (f *os.File, err error) {
+	f, err = os.CreateTemp("", "tmp")
+	if err != nil {
+		return nil, err
+	}
+
+	writer := bufio.NewWriterSize(f, int(bufSize))
+
+	nwrite := 0
+	retry := 32
+	for nwrite < len(data) && retry > 0 {
+		nn, err := writer.Write(data)
+		if err != nil {
+			log.Print(err.Error())
+			retry -= 1
+		}
+		nwrite += nn
+	}
+
+	if err = writer.Flush(); err != nil {
+		return f, err
+	}
+
+	if nwrite < len(data) {
+		return f, fmt.Errorf("Failed to write all data into the file")
+	}
+
+	return f, nil
 }
