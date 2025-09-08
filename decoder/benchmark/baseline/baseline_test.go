60f60c4108f6ddf2402def97770152bfd9406d28
Dekr0 <dekr0.dk@protonmail.com>
2025-09-08 15:14:55 -0600

    - Benchmark test for raw write

--- decoder/benchmark/baseline/baseline_test.go
+++ decoder/benchmark/baseline/baseline_test.go
@@ -55,3 +55,109 @@ func BenchmarkBufferReadLargest(b *testing.B) {
 		baseline.BufferRead(recvSize, bufSize, path)
 	}
 }
+
+func BenchmarkWriteOnceLargest(b *testing.B) {
+	os.Remove("tmp")
+
+	const name = "content_audio_weapons_superearth.st_bnk"
+	path := filepath.Join(SoundBanksDir, name)
+	data, err := os.ReadFile(path)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	b.ResetTimer()
+
+	for b.Loop() {
+		f, err := baseline.WriteOnce(data)
+
+		if err != nil {
+			b.Fatal(err)
+		}
+
+		{
+			b.StopTimer()
+			if f != nil {
+				if err = f.Close(); err != nil {
+					b.Fatal(err)
+				}
+			}
+			if err = os.Remove("tmp"); err != nil {
+				b.Fatal(err)
+			}
+			b.StartTimer()
+		}
+	}
+}
+
+func BenchmarkUnbufferWriteLargest(b *testing.B) {
+	os.Remove("tmp")
+
+	const name = "content_audio_weapons_superearth.st_bnk"
+	const writeSize = 4096
+
+	path := filepath.Join(SoundBanksDir, name)
+	data, err := os.ReadFile(path)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	b.ResetTimer()
+
+	for b.Loop() {
+		f, err := baseline.UnBufferWrite(data, writeSize)
+
+		if err != nil {
+			b.Fatal(err)
+		}
+
+		{
+			b.StopTimer()
+			if f != nil {
+				if err = f.Close(); err != nil {
+					b.Fatal(err)
+				}
+			}
+			if err = os.Remove("tmp"); err != nil {
+				b.Fatal(err)
+			}
+			b.StartTimer()
+		}
+	}
+}
+
+func BenchmarkBufferWriteLargest(b *testing.B) {
+	os.Remove("tmp")
+
+	const name = "content_audio_weapons_superearth.st_bnk"
+	const buffSize = 4096
+
+	path := filepath.Join(SoundBanksDir, name)
+	data, err := os.ReadFile(path)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	b.ResetTimer()
+
+	for b.Loop() {
+		f, err := baseline.BufferWrite(data, buffSize)
+
+		if err != nil {
+			b.Fatal(err)
+		}
+
+		{
+			b.StopTimer()
+			if f != nil {
+				if err = f.Close(); err != nil {
+					b.Fatal(err)
+				}
+			}
+			if err = os.Remove("tmp"); err != nil {
+				b.Fatal(err)
+			}
+			b.StartTimer()
+		}
+	}
+}
