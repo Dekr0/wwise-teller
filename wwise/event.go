d1d4a5f0a5c7921fe8325f61d062047d3f25a57b
Dekr0 <dekr0.dk@protonmail.com>
2025-09-10 08:59:41 -0600

    - Function related to encoding Event, verifying integrity of Event, and calculate the size of Event.

--- wwise/event.go
+++ wwise/event.go
@@ -1,14 +1,97 @@
 package wwise
 
 import (
-	"github.com/Dekr0/unwise/io"
+	bin "encoding/binary"
+	"fmt"
+	"io"
+
+	uio "github.com/Dekr0/unwise/io"
 )
 
 type EventData struct {
-	NumActionIds   io.V128
+	NumActionIds   uio.V128
 	ActionIds    []uint32
 }
 
-type EventComponet struct {
+type EventComponent struct {
 	EventData map[u32]*EventData
+}
+
+func AssertEvent(e *EventComponent, version u32, internalId u32, id u32) {
+	eventData, in := e.EventData[internalId]
+	if !in {
+		panic(fmt.Sprintf("Event %d does not have event data", id))
+	}
+	if eventData.NumActionIds.V != u64(len(eventData.ActionIds)) {
+		panic(fmt.Sprintf("Event %d: # of action ids does not equal to actual # of stored action ids", id))
+	}
+}
+
+func SizeOfEvent(e *EventComponent, version u32, internalId u32, id u32) (size u32) {
+	size = SizeOfHierarchyId
+
+	eventData, in := e.EventData[internalId]
+	if !in {
+		panic(fmt.Sprintf("Event %d does not have event data", id))
+	}
+
+	b, v := eventData.NumActionIds.B, eventData.NumActionIds.V
+
+	size += u32(len(b))
+	size += u32(v) * Size32
+
+	return size
+}
+
+func EncodeEvent(
+	w           io.Writer,
+	o           order,
+	e          *EventComponent,
+	version     u32,
+	internalId  u32, 
+	id          u32,
+) {
+	AssertEvent(e, version, internalId, id)
+
+	var err error
+
+	pos := u32(0)
+
+	size := SizeOfEvent(e, version, internalId, id)
+	eventData, in := e.EventData[internalId]
+	if !in {
+		panic(fmt.Sprintf("Event %d does not have event data", id))
+	}
+
+	header := HierarchyHeader{ HircTypeEvent, size }
+	if err = bin.Write(w, o, header); err != nil {
+		panic(fmt.Errorf("(Event %d) Failed to encode hierarchy header: %w", id, err))
+	}
+
+	if err = bin.Write(w, o, id); err != nil {
+		panic(fmt.Errorf("(Event %d) Failed to encode id: %w", id, err))
+	}
+	pos += SizeOfHierarchyId
+
+	b := eventData.NumActionIds.B
+	n, err := w.Write(b)
+	if err != nil {
+		panic(fmt.Errorf("(Event %d) Failed to write number of action ids: %w", id, err))
+	}
+	if n != len(b) {
+		panic(fmt.Errorf("(Event %d) # of bytes write does not equal to # of bytes stored", id))
+	}
+	pos += u32(len(b))
+
+	actionIds := eventData.ActionIds
+	for i, actionId := range actionIds {
+		if err = bin.Write(w, o, actionId); err != nil {
+			panic(fmt.Errorf("(Event %d) Failed to encode %d-th action id %d", id, i, actionId))
+		}
+		pos += Size32
+	}
+
+	if pos != size {
+		panic(fmt.Errorf("(Event %d) Expect encoded data has a size of %d but receive %d", id, size, pos))
+	}
 }
