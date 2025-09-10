eba908fb5c18c0ceac1c52b9b912ffd9d9a9d39a
Dekr0 <dekr0.dk@protonmail.com>
2025-09-10 08:55:44 -0600

    - more constant value

--- wwise/def.go
+++ wwise/def.go
@@ -1,13 +1,16 @@
 package wwise
 
-const HircIdSize = 4
-
-const HircHeaderSize = Size8 + Size32
-
 const Size8  = 1
 const Size16 = 2
 const Size32 = 4
 const Size64 = 8
+
+const SizeOfHierarchyId = Size32
+const SizeOfActionId    = Size32
+
+const SizeOfHierarchyHeader = Size8 + Size32
+
+const SizeOfHierarchyNumCounter = Size32
 
 type LerpType = u8
 
@@ -28,11 +31,58 @@ const (
 type HircType = uint8
 
 const (
-	HircTypeState  HircType = 0x01
-	HircTypeSound  HircType = 0x02
-	HircTypeAction HircType = 0x03
-	HircTypeEvent  HircType = 0x04
+	HircTypeState               HircType = 0x01
+	HircTypeSound               HircType = 0x02
+	HircTypeAction              HircType = 0x03
+	HircTypeEvent               HircType = 0x04
+	HircTypeRanSeqCntr          HircType = 0x05
+	HircTypeSwitchCntr          HircType = 0x06
+	HircTypeActorMixer          HircType = 0x07
+	HircTypeBus                 HircType = 0x08
+	HircTypeLayerCntr           HircType = 0x09
+	HircTypeMusicSegment        HircType = 0x0a
+	HircTypeMusicTrack          HircType = 0x0b
+	HircTypeMusicSwitchCntr     HircType = 0x0c
+	HircTypeMusicRanSeqCntr     HircType = 0x0d
+	HircTypeAttenuation         HircType = 0x0e
+	HircTypeDialogueEvent       HircType = 0x0f
+	HircTypeFxShareSet          HircType = 0x10
+	HircTypeFxCustom            HircType = 0x11
+	HircTypeAuxBus              HircType = 0x12
+	HircTypeLFOModulator        HircType = 0x13
+	HircTypeEnvelopeModulator   HircType = 0x14
+	HircTypeAudioDevice         HircType = 0x15
+	HircTypeTimeModulator       HircType = 0x16
 )
+
+var HircTypeName [HircTypeTimeModulator]string = [HircTypeTimeModulator]string{
+	"State",
+	"Sound",
+	"Action",
+	"Event",
+	"Random / Sequence Container",
+	"Swithc Container",
+	"Actor Mixer",
+	"Bus",
+	"Layer Container",
+	"Music Segment",
+	"Music Track",
+	"Music Switch Container",
+	"Music Random / Sequence Container",
+	"Attenuation",
+	"Dialogue Event",
+	"FX Share Set",
+	"FX Custom",
+	"Auxiliary Bus",
+	"LFO Modulator",
+	"Envelope Modulator",
+	"Audio Device",
+	"Time Modulator",
+}
+
+func GetHircTypeName(t HircType) string {
+	return HircTypeName[t - 1]
+}
 
 type ChunkName = string
 const (
