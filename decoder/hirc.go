cf075cfe145dbfec3f643748545ccdbc800bb018
Dekr0 <dekr0.dk@protonmail.com>
2025-09-09 22:42:47 -0600

    - wrap a input reader with a limit reader to prevent read out of the boundary of HIRC data chunk - better error message context - Change decoding loop logic flow - the decoding loop will keep running until the following condition meet:   - # of dispatched decoding routine equal to number of hierarchy   - An EOF reach   - An error occur, which will return. - Return an error if # of dispatched is less than to # of hierarchy when hitting an EOF.

--- decoder/hirc.go
+++ decoder/hirc.go
@@ -3,8 +3,8 @@ package decoder
 import (
 	"bytes"
 	"context"
+	"fmt"
 	"io"
-	"sync/atomic"
 
 	uio "github.com/Dekr0/unwise/io"
 	"github.com/Dekr0/unwise/wwise"
@@ -20,38 +20,60 @@ type DecoderOptHIRC struct {
 func DecodeHIRC(
 	ctx      context.Context, 
 	decoder  DecoderOptHIRC,
-	r        io.Reader, 
+	inReader io.Reader, 
 	o        order,
 	size     u32, 
 	ver      u32, 
 ) (h *wwise.HIRC, err error) {
-	p := 0
+	r := io.LimitReader(inReader, int64(size))
 
-	numHirc := uio.U32PT(r, o, &p)
+	numHirc, err := uio.U32(r, o)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to decode # of hierarchies: %w", err)
+	}
 
 	sem := make(chan struct{}, decoder.NumRoutine)
 
 	h = wwise.NewHIRC(numHirc)
 
 	dispatch := uint32(0)
-	parsed := atomic.Uint32{}
 
-	for parsed.Load() < numHirc {
+	eof := false
+	for dispatch < numHirc && !eof {
 		select {
 		case <- ctx.Done():
-			return nil, ctx.Err()
+			return nil, fmt.Errorf("Hierarchy decoding process cancel: %w", ctx.Err())
 		default:
 		}
-		if dispatch >= numHirc {
-			continue
-		}
 
-		t := uio.U8PT(r, o, &p)
-		size := uio.U32PT(r, o, &p)
+		t, err := uio.U8(r, o)
+		if err != nil {
+			if err == io.EOF {
+				eof = true
+				break
+			}
+			return nil, fmt.Errorf("Failed to decode hierarchy type: %w", err)
+		}
+		
+		size, err := uio.U32(r, o)
+		if err != nil {
+			if err == io.EOF {
+				eof = true
+				break
+			}
+			return nil, fmt.Errorf("Failed to decode hierarchy data size: %w", err)
+		}
 
 		buffer := make([]byte, 0, size)
 		if _, err = r.Read(buffer); err != nil {
-			return nil, err
+			if err == io.EOF {
+				eof = true
+				break
+			}
+			return nil, fmt.Errorf(
+				"Failed to read %d bytes of hierarchy data for buffering decoding: %w", 
+				size, err,
+			)
 		}
 		reader := bytes.NewReader(buffer)
 
@@ -70,6 +92,13 @@ func DecodeHIRC(
 			decoder(reader, o, ver, h, size)
 		}
 		dispatch++
+	}
+
+	if dispatch < numHirc && eof {
+		return nil, fmt.Errorf(
+			"Hierarchy decoding process encounter EOF after dispatching %d decoding routine. The total # of hierarchy is %d",
+			dispatch, numHirc,
+		)
 	}
 
 	return h, nil
