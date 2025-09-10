3b5e4534cb921e4116ce4955012d861644d4ad9b
Dekr0 <dekr0.dk@protonmail.com>
2025-09-10 09:00:42 -0600

    - Include test to test all sound banks. - Passing all bytes by bytes test.

--- decoder/bank_test.go
+++ decoder/bank_test.go
@@ -1,25 +1,147 @@
 package decoder_test
 
 import (
+	"bufio"
+	"bytes"
 	"encoding/binary"
+	"fmt"
+	"log/slog"
 	"os"
 	"path/filepath"
 	"testing"
 
 	"github.com/Dekr0/unwise/decoder"
+	"github.com/Dekr0/unwise/wwise"
 )
 
 var SoundBanksDir string = os.Getenv("SOUNDBANKS")
 
-func TestDecode(t *testing.T) {
+func TestDecodeComplex(t *testing.T) {
+	slog.SetDefault(slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{ Level: slog.LevelDebug, AddSource: true, })))
+
+	bankDecodeOpt := decoder.BankDecodeOption{}
+	bankDecodeOpt.ExcludeDATA()
+	hircDecodeOpt := decoder.HircDecodeOption{}
+	hircDecodeOpt.NumRoutine = 0
+
 	const bank = "content_audio_weapons_superearth.st_bnk"
-	_, err := decoder.Decode(
+	bnk, err := decoder.Decode(
 		t.Context(),
 		filepath.Join(SoundBanksDir, bank),
 		binary.LittleEndian,
-		nil,
+		&bankDecodeOpt,
+		&hircDecodeOpt,
 	)
 	if err != nil {
+		t.Fatal(fmt.Errorf("Failed decode %s bank: %w", bank, err))
+	}
+
+	bankEncodeOption := wwise.EncodeBankOpt{}
+	wwise.IncludeEncodedMETA(&bankEncodeOption)
+	hircEncodeOption := wwise.EncodeHircOpt{}
+
+	f, err := os.Create(bank)
+	if err != nil {
+		t.Fatal(fmt.Errorf("Failed to create bank %s: %w", bank, err))
+	}
+	w := bufio.NewWriterSize(f, decoder.PageSize32k)
+
+	err = wwise.EncodeBank(
+		t.Context(), w, binary.LittleEndian, bnk, &bankEncodeOption, &hircEncodeOption,
+	)
+	if err != nil {
+		t.Fatal(fmt.Errorf("Failed to encode bank: %w", err))
+	}
+	if err := w.Flush(); err != nil {
+		t.Fatal(fmt.Errorf("Failed to flush: %w", err))
+	}
+	if err := f.Close(); err != nil {
+		t.Fatal(fmt.Errorf("Failed to close %s: %w", bank, err))
+	}
+
+	data, err := os.ReadFile(bank)
+	if err != nil {
+		t.Fatal(fmt.Errorf("Failed to read data from bank %s: %w", bank, err))
+	}
+	
+	expectData, err := os.ReadFile(filepath.Join(SoundBanksDir, bank))
+	if err != nil {
 		t.Fatal(err)
+	}
+
+	if bytes.Compare(data, expectData) != 0 {
+		t.Fatal("Diff test fail")
+	}
+	
+	os.Remove(bank)
+}
+
+func TestDecodeAll(t *testing.T) {
+	slog.SetDefault(slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{ Level: slog.LevelDebug, AddSource: true, })))
+
+	entries, err := os.ReadDir(SoundBanksDir)
+	if err != nil {
+		t.Fatal(fmt.Errorf("Failed read information about sound bank directory: %w", err))
+	}
+
+	bankDecodeOpt := decoder.BankDecodeOption{}
+	bankDecodeOpt.ExcludeDATA()
+	hircDecodeOpt := decoder.HircDecodeOption{}
+	hircDecodeOpt.NumRoutine = 0
+
+	bankEncodeOption := wwise.EncodeBankOpt{}
+	wwise.IncludeEncodedMETA(&bankEncodeOption)
+	hircEncodeOption := wwise.EncodeHircOpt{}
+
+	for _, entry := range entries {
+		bank := entry.Name()
+		outputBank := filepath.Join("output", bank)
+		inputBank := filepath.Join(SoundBanksDir, bank)
+		t.Run(fmt.Sprintf("Running decoding test on %s", bank), func(t *testing.T) {
+			bnk, err := decoder.Decode(
+				t.Context(),
+				inputBank,
+				binary.LittleEndian,
+				&bankDecodeOpt,
+				&hircDecodeOpt,
+			)
+			if err != nil {
+				t.Fatal(fmt.Errorf("Failed decode %s bank: %w", bank, err))
+			}
+
+			f, err := os.Create(outputBank)
+			if err != nil {
+				t.Fatal(fmt.Errorf("Failed to create bank %s: %w", bank, err))
+			}
+
+			w := bufio.NewWriterSize(f, decoder.PageSize32k)
+			err = wwise.EncodeBank(
+				t.Context(), w, binary.LittleEndian, bnk, &bankEncodeOption, &hircEncodeOption,
+			)
+			if err != nil {
+				t.Fatal(fmt.Errorf("Failed to encode bank: %w", err))
+			}
+			if err := w.Flush(); err != nil {
+				t.Fatal(fmt.Errorf("Failed to flush: %w", err))
+			}
+			if err := f.Close(); err != nil {
+				t.Fatal(fmt.Errorf("Failed to close %s: %w", bank, err))
+			}
+
+			data, err := os.ReadFile(inputBank)
+			if err != nil {
+				t.Fatal(fmt.Errorf("Failed to read data from bank %s: %w", bank, err))
+			}
+
+			expectData, err := os.ReadFile(inputBank)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if bytes.Compare(data, expectData) != 0 {
+				t.Fatal("Diff test fail")
+			}
+
+			os.Remove(outputBank)
+		})
 	}
 }
