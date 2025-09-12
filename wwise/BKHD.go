48e03c756a9c4e3bad2ebd64d46d356f0166160b
Dekr0 <dekr0.dk@protonmail.com>
2025-09-12 02:59:32 -0600

    - replace all "encoding/binary" functions with functions that operate on encoding context data structure

--- wwise/BKHD.go
+++ wwise/BKHD.go
@@ -1,9 +1,9 @@
 package wwise
 
 import (
-	bin "encoding/binary"
 	"fmt"
-	"io"
+
+	uio "github.com/Dekr0/unwise/io"
 )
 
 const BaseSizeBKHD = 20
@@ -32,9 +32,10 @@ func BKHDSize(b *BKHD) u32 {
 	return BaseSizeBKHD + u32(len(b.Data))
 }
 
-func EncodeBKHD(b *BKHD, w io.Writer, o bin.ByteOrder) (err error) {
+func EncodeBKHD(b *BKHD, e *uio.EncoderCtx) (err error) {
 	chunkHeader := ChunkHeader{ [4]byte{'B', 'K', 'H', 'D'}, BKHDSize(b) }
-	if err = bin.Write(w, o, chunkHeader); err != nil {
+	err = uio.EncodeStruct(e, chunkHeader, SizeOfChunkHeader)
+	if err != nil {
 		return fmt.Errorf("Failed to encode BKHD chunk header: %w", err)
 	}
 
@@ -45,11 +46,12 @@ func EncodeBKHD(b *BKHD, w io.Writer, o bin.ByteOrder) (err error) {
 		DeviceAllocatedWithAlignment: (u32(b.Alignment) << 16) | u32(b.DeviceAllocated),
 		Project: b.Project,
 	}
-	if err = bin.Write(w, o, payload); err != nil {
+	err = uio.EncodeStruct(e, payload, Size32 * 5)
+	if err != nil {
 		return fmt.Errorf("Failed to encode BKHD basic field: %w", err)
 	}
 
-	_, err = w.Write(b.Data) 
+	err = uio.EncodeBytes(e, b.Data) 
 	if err != nil {
 		err = fmt.Errorf("Failed to write BKHD encoded data: %w", err)
 	}
