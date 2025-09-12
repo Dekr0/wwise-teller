c44a6d20732825db5f1b1b45bbb1c332c73ee539
Dekr0 <dekr0.dk@protonmail.com>
2025-09-12 02:52:46 -0600

    - view representation of all fields that "might be" used for Base Parameter

--- wwise/base.go
+++ wwise/base.go
@@ -1,13 +1,27 @@
 package wwise
 
-type BaseParamOverride struct {
-	OverrideParentFx         u8
-	OverrideAttachmentParam  u8 // <= 145
-	OverrideParentFxMetadata u8
-	OverrideVector           u8
-	OverrideBusId            u32
-}
+// --- struct definition --- // 
 
-type ParentId struct {
-	ParentId u32
+// This is only used for read. How data is stored is completely different
+type BaseParameterS struct {
+	OverrideParentFx         u8
+	OverrideParentFxMetadata u8
+	OverrideAttachmentParam    u8
+	SettingVector            u8
+	AdvanceSettingVector     u8
+	VirtualQueueBehavior     u8
+	BelowThresholdBehavior   u8
+	MaxNumInstance           u16
+	OverideBusId             u32
+	DirectParentId           u32
+	FXs                     *FXs
+	FxMetadatas             *FxMetadatas
+	Prop                    *Prop
+	RProp                   *RProp
+	PositionParam           *PositionParam
+	AuxParam                *AuxParam
+	StateProp               *StateProp
+	StateGroup              *StateGroup
+	RTPC                     []RTPC
+	RTPCGraph                []RTPCGraph
 }
