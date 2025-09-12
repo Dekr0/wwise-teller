c6e12187e8534e022c21639fc331c120ac455381
Dekr0 <dekr0.dk@protonmail.com>
2025-09-12 03:10:51 -0600

    - rename NewEvent ot AddEvent - Prefix Alloc to DecodeEvent to indicate this decoding function will allocate memory behind the scence by the runtime.

--- decoder/event.go
+++ decoder/event.go
@@ -9,16 +9,13 @@ import (
 
 // Expect r to be SectionReader, LimitReader, bytes.Reader, or bytes.Buffer 
 // since these type of reader will do automatic bound checking.
-func DecodeEvent(r io.Reader, o order, ver u32, h *wwise.HIRC, size u32) {
+func AllocDecodeEvent(r io.Reader, o order, ver u32, h *wwise.HIRC, size u32) {
 	id := uio.U32P(r, o)
 
-	data := &wwise.EventData{}
-	data.NumActionIds = *uio.VV128P(r, o)
-	v := data.NumActionIds.V
-	data.ActionIds = make([]u32, v, v)
-	for i := range v {
+	data := wwise.AllocEventData(uio.VV128P(r, o))
+	for i := range data.ActionIds {
 		data.ActionIds[i] = uio.U32P(r, o)
 	}
 
-	wwise.NewEvent(h, id, data)
+	wwise.AddEvent(h, id, data)
 }
