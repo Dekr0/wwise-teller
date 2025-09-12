7d6b058375a8d25f9421a775d42effee17cb3da5
Dekr0 <dekr0.dk@protonmail.com>
2025-09-12 02:44:47 -0600

    - comment annotation to divide code - function "allocate" Event data structure - rename `NewEventData` to `AddEventData` - pass in hierarchy encoder context instead of individual fields related to context of encoding - replace all "encoding/binary" functions with functions that operate on encoding context data structure

--- wwise/event.go
+++ wwise/event.go
@@ -1,13 +1,13 @@
 package wwise
 
 import (
-	bin "encoding/binary"
 	"fmt"
-	"io"
 	"sync"
 
 	uio "github.com/Dekr0/unwise/io"
 )
+
+// --- struct definition --- //
 
 type EventData struct {
 	NumActionIds   uio.V128
@@ -20,7 +20,18 @@ type EventComponent struct {
 	EventData map[u32]*EventData
 }
 
-func NewEventData(e *EventComponent, internalId u32, data *EventData) {
+// --- allocation / freeing --- //
+
+func AllocEventData(numActionIds *uio.V128) (data *EventData) {
+	return &EventData{
+		NumActionIds: *numActionIds,
+		ActionIds: make([]uint32, numActionIds.V, numActionIds.V),
+	}
+}
+
+// --- function --- //
+
+func AddEventData(e *EventComponent, internalId u32, data *EventData) {
 	if data == nil {
 		panic("Event data is nil")
 	}
@@ -60,54 +71,45 @@ func SizeOfEvent(e *EventComponent, version u32, internalId u32, id u32) (size u
 }
 
 func EncodeEvent(
-	w           io.Writer,
-	o           order,
+	eCtx       *HircEncoderCtx,
 	e          *EventComponent,
-	version     u32,
 	internalId  u32, 
 	id          u32,
 ) {
-	AssertEvent(e, version, internalId, id)
+	AssertEvent(e, eCtx.Version, internalId, id)
 
 	var err error
 
-	pos := u32(0)
-
-	size := SizeOfEvent(e, version, internalId, id)
+	size := SizeOfEvent(e, eCtx.Version, internalId, id)
 	eventData, in := e.EventData[internalId]
 	if !in {
 		panic(fmt.Sprintf("Event %d does not have event data", id))
 	}
 
 	header := HierarchyHeader{ HircTypeEvent, size }
-	if err = bin.Write(w, o, header); err != nil {
+	if err = uio.EncodeStruct(eCtx.Encoder, header, SizeOfHierarchyHeader); err != nil {
 		panic(fmt.Errorf("(Event %d) Failed to encode hierarchy header: %w", id, err))
 	}
 
-	if err = bin.Write(w, o, id); err != nil {
+	curr := eCtx.Encoder.Count
+
+	if err = HIRCEncode(eCtx, id); err != nil {
 		panic(fmt.Errorf("(Event %d) Failed to encode id: %w", id, err))
 	}
-	pos += SizeOfHierarchyId
 
-	b := eventData.NumActionIds.B
-	n, err := w.Write(b)
+	err = HIRCEncodeBytes(eCtx, eventData.NumActionIds.B)
 	if err != nil {
 		panic(fmt.Errorf("(Event %d) Failed to write number of action ids: %w", id, err))
 	}
-	if n != len(b) {
-		panic(fmt.Errorf("(Event %d) # of bytes write does not equal to # of bytes stored", id))
-	}
-	pos += u32(len(b))
 
 	actionIds := eventData.ActionIds
 	for i, actionId := range actionIds {
-		if err = bin.Write(w, o, actionId); err != nil {
+		if err = HIRCEncode(eCtx, actionId); err != nil {
 			panic(fmt.Errorf("(Event %d) Failed to encode %d-th action id %d", id, i, actionId))
 		}
-		pos += Size32
 	}
 
-	if pos != size {
-		panic(fmt.Errorf("(Event %d) Expect encoded data has a size of %d but receive %d", id, size, pos))
+	if err := HIRCAssertEncodeLimit(eCtx, curr, size); err != nil {
+		panic(fmt.Errorf("(Event %d) %w", id, err))
 	}
 }
