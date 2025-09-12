aeaf648713e55d6351b213085bafcf3954494781
Dekr0 <dekr0.dk@protonmail.com>
2025-09-12 03:43:10 -0600

    - Prefix Alloc to DecodeEvent to indicate this decoding function will allocate memory behind the scence by the runtime. - Replace "New" with "Add" to give better meaning of what the function is doing since it is accepting parameter that is already allocate somewhere else, and registering into the system. "New" might give the meaning of both allocating and registering. - Rename wwise.DIDXDATA to wwise.AudioStore

--- decoder/didx.go
+++ decoder/didx.go
@@ -8,26 +8,26 @@ import (
 	"github.com/Dekr0/unwise/wwise"
 )
 
-func DecodeDIDX(
+func AllocDecodeDIDX(
 	inReader  io.Reader,
 	chunkSize u32,
 	o         order,
 ) (
-	d *wwise.DIDXDATA, err error,
+	d *wwise.AudioStore, err error,
 ) {
 	num := chunkSize / wwise.SizeOfMediaIndex
 
 	r := io.LimitReader(inReader, int64(chunkSize))
 
-	d = wwise.NewDIDXDATA(chunkSize)
+	d = wwise.AllocAudioStore(chunkSize)
 
-	var m wwise.MediaIndexEntry
+	var m wwise.MediaIndexS
 	for i := range num {
 		err = uio.Struct(r, o, &m)
 		if err != nil {
 			return nil, fmt.Errorf("Failed to decode media index entry at %d: %w", i, err)
 		}
-		wwise.NewMediaIndex(d, m)
+		wwise.AddMediaIndex(d, m)
 	}
 
 	return d, nil
