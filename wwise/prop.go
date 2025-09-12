594fb6ff0c727ad21ec41b2b45897037bf673af6
Dekr0 <dekr0.dk@protonmail.com>
2025-09-12 02:43:35 -0600

    - complete struct definition of property bundle and range based property bundle

--- wwise/prop.go
+++ wwise/prop.go
@@ -1,7 +1,28 @@
 package wwise
 
+import "sync"
+
+// --- struct definition --- //
+
 type Prop struct {
+	Ids  []u32
+	Vals []byte
+}
+
+type PropComponent struct {
+	dMu sync.Mutex
+
+	ActorMixerProp map[u32]*PropComponent
 }
 
 type RProp struct {
+	Ids  []u32
+	Mins []byte
+	Maxs []byte
+}
+
+type RPropComponent struct {
+	dMu sync.Mutex
+
+	ActorMixerRProp map[u32]*RProp
 }
