d187ecef5119fbd1c92d11356b06c7558c41e6ef
Dekr0 <dekr0.dk@protonmail.com>
2025-09-14 09:15:38 -0600

    - Remove the use of mutex - SizeOfEvent and AssertEvent do not accept version - EncodeEvent will not call AssertEvent - Function to "allocate" EventComponent - Presentation layer struct of Event

--- wwise/event.go
+++ wwise/event.go
@@ -2,12 +2,15 @@ package wwise
 
 import (
 	"fmt"
-	"sync"
-
 	uio "github.com/Dekr0/unwise/io"
 )
 
 // --- struct definition --- //
+
+type EventH struct {
+	Id         u32
+	EventData *EventData
+}
 
 type EventData struct {
 	NumActionIds   uio.V128
@@ -15,12 +18,21 @@ type EventData struct {
 }
 
 type EventComponent struct {
-	dMu sync.Mutex
-
 	EventData map[u32]*EventData
 }
 
 // --- allocation / freeing --- //
+
+func AllocEventComponent(numEvent u32) *EventComponent {
+	if numEvent <= 0 {
+		return &EventComponent{
+			EventData: make(map[u32]*EventData),
+		}
+	}
+	return &EventComponent{
+		EventData: make(map[u32]*EventData, numEvent),
+	}
+}
 
 func AllocEventData(numActionIds *uio.V128) (data *EventData) {
 	return &EventData{
@@ -31,20 +43,19 @@ func AllocEventData(numActionIds *uio.V128) (data *EventData) {
 
 // --- function --- //
 
+// Has side effect
 func AddEventData(e *EventComponent, internalId u32, data *EventData) {
 	if data == nil {
 		panic("Event data is nil")
 	}
-
-	e.dMu.Lock()
-	defer e.dMu.Unlock()
 	if _, in := e.EventData[internalId]; in {
 		panic(MonotonicIdCollision)
 	}
 	e.EventData[internalId] = data
 }
 
-func AssertEvent(e *EventComponent, version u32, internalId u32, id u32) {
+// Has no side effect
+func AssertEvent(e *EventComponent, internalId u32, id u32) {
 	eventData, in := e.EventData[internalId]
 	if !in {
 		panic(fmt.Sprintf("Event %d does not have event data", id))
@@ -54,61 +65,50 @@ func AssertEvent(e *EventComponent, version u32, internalId u32, id u32) {
 	}
 }
 
-func SizeOfEvent(e *EventComponent, version u32, internalId u32, id u32) (size u32) {
-	size = SizeOfHierarchyId
-
+// Has no side effect
+func SizeOfEvent(e *EventComponent, internalId u32, id u32) (size u32) {
 	eventData, in := e.EventData[internalId]
 	if !in {
 		panic(fmt.Sprintf("Event %d does not have event data", id))
 	}
-
+	size = SizeOfHierarchyId
 	b, v := eventData.NumActionIds.B, eventData.NumActionIds.V
-
 	size += u32(len(b))
 	size += u32(v) * Size32
-
 	return size
 }
 
+// Has no side effect
 func EncodeEvent(
 	eCtx       *HircEncoderCtx,
 	e          *EventComponent,
 	internalId  u32, 
 	id          u32,
 ) {
-	AssertEvent(e, eCtx.Version, internalId, id)
-
 	var err error
-
-	size := SizeOfEvent(e, eCtx.Version, internalId, id)
+	size := SizeOfEvent(e, internalId, id)
 	eventData, in := e.EventData[internalId]
 	if !in {
 		panic(fmt.Sprintf("Event %d does not have event data", id))
 	}
-
 	header := HierarchyHeader{ HircTypeEvent, size }
 	if err = uio.EncodeStruct(eCtx.Encoder, header, SizeOfHierarchyHeader); err != nil {
 		panic(fmt.Errorf("(Event %d) Failed to encode hierarchy header: %w", id, err))
 	}
-
 	curr := eCtx.Encoder.Count
-
 	if err = HIRCEncode(eCtx, id); err != nil {
 		panic(fmt.Errorf("(Event %d) Failed to encode id: %w", id, err))
 	}
-
 	err = HIRCEncodeBytes(eCtx, eventData.NumActionIds.B)
 	if err != nil {
 		panic(fmt.Errorf("(Event %d) Failed to write number of action ids: %w", id, err))
 	}
-
 	actionIds := eventData.ActionIds
 	for i, actionId := range actionIds {
 		if err = HIRCEncode(eCtx, actionId); err != nil {
 			panic(fmt.Errorf("(Event %d) Failed to encode %d-th action id %d", id, i, actionId))
 		}
 	}
-
 	if err := HIRCAssertEncodeLimit(eCtx, curr, size); err != nil {
 		panic(fmt.Errorf("(Event %d) %w", id, err))
 	}
